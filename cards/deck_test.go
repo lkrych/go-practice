39713d3d16c3df52c321f5115a0e004791b17108
Leland Krych <Leland.Krych@gmail.com>
2017-09-17 11:51:55 -0700

    create i/o tests for deck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,20 @@ func TestNewDeck(t *testing.T) {
 	if d[51] != "King of Clubs" {
 		t.Errorf("Expected last card to be King of Clubs, but got %v", d[51])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+
+	os.Remove("_decktesting")
+
 }
