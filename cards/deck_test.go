d0e94c6d60031e85edd3d7c6cea49cc86c831202
Leland Krych <Leland.Krych@gmail.com>
2017-09-17 11:41:23 -0700

    finish tests for newDeck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of spades, but got %v", d[0])
+	}
+
+	if d[51] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs, but got %v", d[51])
+	}
 }
