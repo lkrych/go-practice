f07abfee4ed8bc573b58a137c1eb58522c776c7c
Leland Krych <Leland.Krych@gmail.com>
2017-09-20 15:24:00 -0700

    nest structs

--- structs/main.go
+++ structs/main.go
@@ -5,13 +5,23 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zip   int
 }
 
 func main() {
 	var alex person
+	var alexContact contactInfo
 
 	alex.firstName = "Alex"
 	alex.lastName = "Anderson"
+	alex.contact = alexContact
+	alex.contact.email = "alex@glad.com"
+	alex.contact.zip = 11111
 
 	fmt.Println(alex)
 	fmt.Printf("%+v", alex)
