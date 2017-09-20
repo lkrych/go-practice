94b2a0ead2be4baa79fe0ba4cc2f56362d7bb628
Leland Krych <Leland.Krych@gmail.com>
2017-09-20 15:59:37 -0700

    add another anotation

--- structs/pointers.go
+++ structs/pointers.go
@@ -2,5 +2,6 @@ package main
 
 func (pointerToPerson *person) updateNamePointer(newFirstName string) {
 	(*pointerToPerson).firstName = newFirstName
-	//the star function asks for the value at the memory address, this will be the person struct
+	//the star operator asks for the value at the memory address, this will be the person struct
+	//the star in front of the type is a type description
 }
