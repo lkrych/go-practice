a95d2300cf9db04f90d1231affb5eea108c58b07
Leland Krych <Leland.Krych@gmail.com>
2017-09-28 08:48:17 -0700

    add nthPrime, isPrime and scrambledString

--- exercises/exercises.go
+++ exercises/exercises.go
@@ -214,3 +214,33 @@ func mostCommonLetter(sentence string) []string {
 func capitalizeWords(sentence string) string {
 	return strings.Title(sentence)
 }
+
+func scrambleString(word string, idxArray []int) string {
+	split := strings.Split(word, "")
+	scrambled := []string{}
+	for _, el := range idxArray {
+		scrambled = append(scrambled, split[el])
+	}
+	return strings.Join(scrambled, "")
+}
+
+func isPrime(n int) bool {
+	for i := 2; i < n/2+1; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func nthPrime(nth int) int {
+	primeCount := 0
+	n := 2
+	for primeCount < nth {
+		if isPrime(n) {
+			primeCount++
+		}
+		n++
+	}
+	return n - 1
+}
--- exercises/exercises_test.go
+++ exercises/exercises_test.go
@@ -367,10 +367,10 @@ func TestCapitalizeWords(t *testing.T) {
 }
 
 func TestScrambleString(t *testing.T) {
-	test1 := scrambleString("abcd", [3,1,2,0])
-	test2 := scrambleString("markov", [5,3,1,4,2,0])
+	test1 := scrambleString("abcd", []int{3, 1, 2, 0})
+	test2 := scrambleString("markov", []int{5, 3, 1, 4, 2, 0})
 
-	if test1 != "dcba" {
+	if test1 != "dbca" {
 		t.Errorf("Expected scrambleString of 'abcd' with inputs [3,1,2,0] to be 'dcba', not %v", test1)
 	}
 
@@ -384,7 +384,7 @@ func TestIsPrime(t *testing.T) {
 	test2 := isPrime(3)
 	test3 := isPrime(4)
 	test4 := isPrime(9)
-	test5:= isPrime(227)
+	test5 := isPrime(227)
 
 	if test1 != true {
 		t.Errorf("Expected isPrime of 2 to be true, not false")
@@ -405,4 +405,32 @@ func TestIsPrime(t *testing.T) {
 	if test5 != true {
 		t.Errorf("Expected isPrime of 227 to be true, not false")
 	}
-)
+}
+
+func TestNthPrime(t *testing.T) {
+	test1 := nthPrime(3)
+	test2 := nthPrime(4)
+	test3 := nthPrime(15)
+	test4 := nthPrime(28)
+	test5 := nthPrime(77)
+
+	if test1 != 5 {
+		t.Errorf("Expected nthPrime of 3 to be 5, not %v", test1)
+	}
+
+	if test2 != 7 {
+		t.Errorf("Expected nthPrime of 4 to be 7, not %v", test2)
+	}
+
+	if test3 != 47 {
+		t.Errorf("Expected nthPrime of 15 to be 47, not %v", test3)
+	}
+
+	if test4 != 107 {
+		t.Errorf("Expected nthPrime of 28 to be 107, not %v", test4)
+	}
+
+	if test5 != 389 {
+		t.Errorf("Expected nthPrime of 77 to be 389, not %v", test5)
+	}
+}
