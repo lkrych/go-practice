08c558d1e28c50e6fd42e012bfcd732004d85065
Leland Krych <Leland.Krych@gmail.com>
2017-09-29 08:58:32 -0700

    update the index route handler

--- go_web/chitchat/main.go
+++ go_web/chitchat/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
@@ -33,16 +32,4 @@ func main() {
 		Handler: mux,
 	}
 	server.ListenAndServe()
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html"}
-	templates := template.Must(template.ParseFiles(files...))
-	threads, err := data.Threads()
-	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
 }
---
+++ go_web/chitchat/util.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+)
+
+//checks if user is logged in and has a session, if not err is not nil
+func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
+	cookie, err := request.Cookie("_cookie")
+	if err == nil {
+		sess = data.Session{Uuid: cookie.Value}
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
