25c14aced8385731ea5ea8aeac64b8f0c313589e
Leland Krych <Leland.Krych@gmail.com>
2017-10-01 18:59:11 -0700

    chain handler functions

--- go_web/server/main.go
+++ go_web/server/main.go
@@ -3,27 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
 
-type HelloHandler struct{}
-
-func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
-type WorldHandler struct{}
-
-func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "World!")
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		h(w, r)
+	}
 }
 
 func main() {
-	hello := HelloHandler{}
-	world := WorldHandler{}
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
+	http.HandleFunc("/hello", log(hello))
+
 	server.ListenAndServe()
 }
