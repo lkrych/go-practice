8036d609fedd0470cd1f1da01be80c61564a4580
Leland Krych <Leland.Krych@gmail.com>
2017-10-02 17:26:20 -0700

    add method chaining and logggging to httprouter

--- go_web/router/main.go
+++ go_web/router/main.go
@@ -11,9 +11,16 @@ func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
 }
 
+func log(h httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Printf("hello was called with %s\n", p.ByName("name"))
+		h(w, r, p)
+	}
+}
+
 func main() {
 	mux := httprouter.New()
-	mux.GET("/hello/:name", hello)
+	mux.GET("/hello/:name", log(hello))
 
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
