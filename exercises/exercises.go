7cd9ab7eb9ecf2ec7f1b5d587ddc08dcb7d22198
Leland Krych <Leland.Krych@gmail.com>
2017-10-04 17:36:48 -0700

    bubbley

--- exercises/exercises.go
+++ exercises/exercises.go
@@ -370,3 +370,17 @@ func binarySearch(arr []int, toFind int) int {
 	}
 
 }
+
+func bubbleSort(arr []int) []int {
+	sorted := false
+	for !sorted {
+		sorted = true
+		for i := 0; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				sorted = false
+			}
+		}
+	}
+	return arr
+}
