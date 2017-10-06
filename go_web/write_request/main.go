7e523e8cf386bde1504771218ce131f75fcc4d6e
Leland Krych <Leland.Krych@gmail.com>
2017-10-05 21:48:45 -0700

    add writeHeader and header examples

--- go_web/write_request/main.go
+++ go_web/write_request/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
@@ -11,10 +14,22 @@ func writeExample(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(str))
 }
 
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "No such service, try next door")
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
 	server.ListenAndServe()
 }
