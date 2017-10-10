b20ea3b7370d7ca1558bbb3c3259decee118bcb6
Leland Krych <Leland.Krych@gmail.com>
2017-10-10 16:40:47 -0700

    add quicksort to exercises

--- exercises/exercises.go
+++ exercises/exercises.go
@@ -421,3 +421,28 @@ func merge(left []int, right []int) []int {
 	sorted = append(sorted, right...)
 	return sorted
 }
+
+func quickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	pivot := arr[0]
+	left := []int{}
+	right := []int{}
+
+	for _, val := range arr[1:] {
+		if val >= pivot {
+			right = append(right, val)
+		} else {
+			left = append(left, val)
+		}
+	}
+	answer := []int{}
+	sortedLeft := quickSort(left)
+	sortedRight := quickSort(right)
+	answer = append(answer, sortedLeft...)
+	answer = append(answer, pivot)
+	answer = append(answer, sortedRight...)
+	return answer
+}
