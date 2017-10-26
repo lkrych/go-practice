1b1030a60c4412222861ce4697f9da61d84594f0
Leland Krych <Leland.Krych@gmail.com>
2017-10-25 20:26:53 -0700

    hash user input using md5 pkg

--- identicon/main.go
+++ identicon/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"os"
+)
 
+func main() {
+	hashedInput := readAndHashInput()
+	color := hashedInput[:3]
+	fmt.Println(hashedInput)
+	fmt.Println("The color is:", color)
+}
+
+func readAndHashInput() [16]byte {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text to create an identicon: ")
+	text, _ := reader.ReadString('\n')
+	return md5.Sum([]byte(text))
 }
