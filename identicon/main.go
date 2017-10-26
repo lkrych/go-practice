9657ce69882a7a7bebb421d16a18062623dc5984
Leland Krych <Leland.Krych@gmail.com>
2017-10-25 22:16:35 -0700

    finish identicon by drawing to local png file

--- identicon/main.go
+++ identicon/main.go
@@ -4,18 +4,45 @@ import (
 	"bufio"
 	"crypto/md5"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
 	"os"
 )
 
 func main() {
 	hashedInput := readAndHashInput()
-	color := hashedInput[:3]
+	c := hashedInput[:3]
+	colorDraw := color.RGBA{c[0], c[1], c[2], 255}
 	grid := buildGrid(hashedInput)
 	filtered := filterOddSquares(grid)
-	fmt.Println(hashedInput)
-	fmt.Println("The color is:", color)
-	fmt.Println("The grid is ", grid)
-	fmt.Println("The filtered grid is ", filtered)
+
+	newPNGfile := "./identicon.png"
+
+	myimage := image.NewRGBA(image.Rect(0, 0, 250, 250))
+	white := color.RGBA{255, 255, 255, 255}
+
+	// backfill entire surface with white
+	draw.Draw(myimage, myimage.Bounds(), &image.Uniform{white}, image.ZP, draw.Src)
+
+	for idx, el := range filtered {
+		if el == 0 {
+			continue
+		}
+		topX := (idx % 5) * 50
+		botX := topX + 50
+		topY := (idx / 5) * 50
+		botY := topY + 50
+
+		rect := image.Rect(topX, topY, botX, botY)
+		draw.Draw(myimage, rect, &image.Uniform{colorDraw}, image.ZP, draw.Src)
+	}
+
+	// ... save image
+
+	myfile, _ := os.Create(newPNGfile)
+	png.Encode(myfile, myimage)
 }
 
 func readAndHashInput() [16]byte {
