18a11c2e734c9ac4c3172bcb58f2874b2b8640b0
Leland Krych <Leland.Krych@gmail.com>
2017-11-05 13:23:08 -0800

    demonstrate fallthrough keyword

--- udemy/modern_go/eloquent_go/building_blocks.go
+++ udemy/modern_go/eloquent_go/building_blocks.go
@@ -5,17 +5,42 @@ import (
 )
 
 func main() {
-	fmt.Println(CompareNumbers(1, 1))
+	fmt.Println(compareNumbers(1, 1))
+	x := 2
+	switch x {
+	case 3:
+		fmt.Println("I am 3")
+	case 2:
+		fmt.Println("I am 2")
+		//fallthrough // evaluates the next statement/s as true!
+	case 4:
+		fmt.Println("I am 4")
+	}
 }
 
-func CompareNumbers(i1, i2 int) (bool, int) {
-	if i1 > i2 {
+func compareNumbers(i1, i2 int) (bool, int) {
+	/*
+		if i1 > i2 {
+			fmt.Println("the first number is greater than the second number")
+			return false, i1 - i2
+		} else if i2 > i1 {
+			fmt.Println("The second number is greater than the first number")
+			return false, i2 - i1
+		}
+		fmt.Println("The numbers are equal!")
+		return true, 0
+	*/
+
+	//alternative syntax for conditionals
+	switch {
+	case i1 > i2:
 		fmt.Println("the first number is greater than the second number")
 		return false, i1 - i2
-	} else if i2 > i1 {
+	case i2 > i1:
 		fmt.Println("The second number is greater than the first number")
 		return false, i2 - i1
 	}
 	fmt.Println("The numbers are equal!")
 	return true, 0
+
 }
