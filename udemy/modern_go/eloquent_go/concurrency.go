12e23ef1bb73e729ea05bc712708dc97162ff53b
Leland Krych <Leland.Krych@gmail.com>
2017-11-06 21:29:47 -0800

    add examples for goroutines, channels and close

--- udemy/modern_go/eloquent_go/concurrency.go
+++ udemy/modern_go/eloquent_go/concurrency.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
@@ -12,14 +15,42 @@ func main() {
 	//goroutines get scheduled and multiplexed amongst open OS threads
 
 	//a channel is a way to communicate between go routines
-	qs := make(chan bool)
-	go SayHelloFromGoRoutine(qs)
-	fmt.Println("Hello from main")
-	v := <-qs      //wait on channel and listen for incoming value
-	fmt.Println(v) // prints true
+	// qs := make(chan bool)
+	// // go SayHelloFromGoRoutine(qs)
+	// go func() { //use anonymous function to do same thing as SayHello...
+	// 	fmt.Println("Hello from a third goroutine!")
+	// 	qs <- false //captured via closure
+	// }()
+	// fmt.Println("Hello from main")
+	// v := <-qs      //wait on channel and listen for incoming value
+	// fmt.Println(v) // prints true
+
+	// select
+	ic := make(chan int)
+	go periodicSend(ic)
+	for i := range ic { //continuously listen to ic channel and intercept val
+		fmt.Println(i)
+	}
+	_, ok := <-ic
+	if ok {
+		ic <- 3 //will panic because the channel is closed!
+	} else {
+		fmt.Println("The channel is closed")
+	}
+
 }
 
 func SayHelloFromGoRoutine(qs chan bool) {
 	fmt.Println("Hello from a new goroutine")
 	qs <- true
+}
+
+func periodicSend(ic chan int) {
+	i := 0
+	for i <= 3 {
+		ic <- i
+		i++
+		time.Sleep(1 * time.Second)
+	}
+	close(ic) //tell goroutine in main to stop listening
 }
