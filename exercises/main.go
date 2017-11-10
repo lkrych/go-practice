aea2e6efec8af7bf2bb76b60adf76dcebf39ff9e
Leland Krych <Leland.Krych@gmail.com>
2017-11-09 21:59:21 -0800

    add func to read and create test files for exercises_test directory

--- exercises/main.go
+++ exercises/main.go
@@ -2,9 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	potato := countVowels("potato")
-	fmt.Println("the number of vowels in potato is", potato)
+	readAndCreateFiles("exercises_itemized")
+}
+
+func readAndCreateFiles(directory string) {
+	//create test files for all exercises in directory
+	files, err := ioutil.ReadDir(fmt.Sprintf("./%v", directory))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		split := strings.Split(f.Name(), ".")
+		testName := fmt.Sprintf("%v_test.go", split[0])
+		f, err := os.Create(fmt.Sprintf("./exercises_test/%v", testName))
+		check(err)
+		defer f.Close()
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
