e52cee1240bdac3448df888155ec28299af35081
Leland Krych <Leland.Krych@gmail.com>
2017-11-15 10:53:05 -0800

    add increment arbitrary to exercise suite

---
+++ exercises/exercises_itemized/increment_arbitrary.go
@@ -0,0 +1,8 @@
+//take in an array of digits encoding a decimal, d and update the array to rep d + 1
+//ex: 1.29 => 1.30 ~ [1,2,9] => [1,3,0]
+//ex 1.48372 => 1.48373 
+//TAKE AWAY:  This algorithm should work in a language that has finite-precision arithmetic
+//Arrays can be used to break up numbers of arbitrary size, this allows us to handle very big numbers!
+func incrementArb(arr []int) []int {
+	
+}
