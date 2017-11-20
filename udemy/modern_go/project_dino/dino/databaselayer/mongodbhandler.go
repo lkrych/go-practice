6a77b19cbed99f0ebdd16fc14e4f6e204c8ccafb
Leland Krych <Leland.Krych@gmail.com>
2017-11-19 22:03:32 -0800

    add mongo layer for animal project

---
+++ udemy/modern_go/project_dino/dino/databaselayer/mongodbhandler.go
@@ -0,0 +1,55 @@
+package databaselayer
+
+import mgo "gopkg.in/mgo.v2"
+
+type MongodbHandler struct {
+	*mgo.Session
+}
+
+func NewMongdbHandler(connection string) (*MongodbHandler, error) {
+	s, err := mgo.Dial(connection)
+	return &MongodbHandler{
+		Session: s,
+	}, err
+}
+
+func (handler *MongodbHandler) GetAvailableAnimals() ([]Animal, error) {
+	s := handler.getFreshSession()
+	defer s.Close()
+	animals := []Animal{}
+	err := s.DB("Dino").C("animals").Find(nil).All.(&animals)
+	return animals, err
+}
+
+func (handler *MongodbHandler) GetAnimalByNickname(nickname string) (Animal, error) {
+	s := handler.getFreshSession()
+	defer s.Close()
+	a := Animal{}
+	err := s.DB("Dino").C("animals").Find(bson.M{"nickname":nickname}).One(&a)
+	return a, err
+}
+
+func (handler *MongodbHandler) GetAnimalsByTYpe(animalType string) ([]Animal, error) {
+	s := handler.getFreshSession()
+	defer s.Close()
+	animals := []Animal{}
+	err := s.DB("Dino").C("animals").Find(bson.M{"animal_type": animalType}).All(&animals)
+	return animals, err
+}
+
+func (handler *MongodbHandler) AddAnimal(a Animal) error {
+	s := handler.getFreshSession()
+	defer s.Close()
+	return s.DB("Dino").C("animals").Insert(a)
+}
+
+func (handler *MongodbHandler) UpdateAnimal(a Animal, nname string) error {
+	s := handler.getFreshSession()
+	defer s.Close()
+	return s.DB("Dino").C("animals").Update(bson.M{"nickname": nname}, a)
+}
+
+//obtains another session to the same database from a connection pool
+func (handler *MongodbHandler) getFreshSession() *mgo.Session {
+	return handler.Session.Copy()
+}
