207fee1b5a7276607bfa8c636c518b603bacd246
Leland Krych <Leland.Krych@gmail.com>
2017-11-29 08:09:35 -0800

    move playground files into practice folder to avoid conflict with exercise_generator

--- exercises/practice/exercises.go
+++ exercises/practice/exercises.go
@@ -527,3 +527,14 @@ func advancingArray(arr []int) bool {
 	}
 	return false
 }
+
+func removeDups(arr []int) []int {
+	writeIdx := 1
+	for i := 1; i < len(arr); i++ {
+		if arr[writeIdx-1] != arr[i] {
+			arr[writeIdx] = arr[i]
+			writeIdx++
+		}
+	}
+	return arr[:writeIdx]
+}
