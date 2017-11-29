bc31ef0c911d7ae3ca27a45235eb339c4a1b9315
Leland Krych <Leland.Krych@gmail.com>
2017-11-29 08:37:14 -0800

    add buyStock to solutions

--- exercises/practice/exercises.go
+++ exercises/practice/exercises.go
@@ -538,3 +538,17 @@ func removeDups(arr []int) []int {
 	}
 	return arr[:writeIdx]
 }
+
+func buyStock(arr []int) int {
+	low := 1000000
+	diff := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < low {
+			low = arr[i]
+		}
+		if (arr[i] - low) > diff {
+			diff = arr[i] - low
+		}
+	}
+	return diff
+}
