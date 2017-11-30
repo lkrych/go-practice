4c016075555b9975347c6a1711d42304d546f730
Leland Krych <Leland.Krych@gmail.com>
2017-11-30 08:38:23 -0800

    add buy_stock_twice to exercises and tests

---
+++ exercises/exercises_itemized/buy_stock_twice.go
@@ -0,0 +1,5 @@
+//given an arr representing stock prices, return the maximum profit you could retrieve from buying and selling the stock twice
+//ex: [310, 315, 275, 295, 260, 270, 290, 230, 255, 250] => 50
+func buyStockTwice(arr []int) int {
+
+}
