d17bb070420564cb5f9eb41137c3ea2624ca15e2
Leland Krych <Leland.Krych@gmail.com>
2017-12-01 10:38:02 -0800

    add reverse_captcha

---
+++ exercises/exercises_itemized/reverse_captcha.go
@@ -0,0 +1,12 @@
+//from day 1 of coding advent calendar
+// find the sum of all digits that match the next digit in the list. 
+//The list is circular, so the digit after the last digit is the first digit in the list.
+func reverseCaptcha(arr []int) int {
+	
+}
+	
+func adventCaptcha() int {
+	//read in big int from "practice/reverse_captcha_input.txt"
+	//convert it into something useful and then return the sum
+}
+	
