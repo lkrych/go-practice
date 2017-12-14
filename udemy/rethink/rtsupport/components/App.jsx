88d30c233b54d0125fac4c76d6f5a5dbcff5f47d
Leland Krych <Leland.Krych@gmail.com>
2017-12-14 14:12:58 -0800

    add Messages to App.jsx

--- udemy/rethink/rtsupport/components/App.jsx
+++ udemy/rethink/rtsupport/components/App.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
+import UserSection from './users/UserSection.jsx';
+import MessageSection from './messages/MessageSection.jsx';
 
 class App extends Component {
   constructor(props){
@@ -7,6 +9,7 @@ class App extends Component {
     this.state = {
       channels: [],
       users: [],
+      messages: [],
       activeChannel: {}
     };
   }
@@ -23,6 +26,13 @@ class App extends Component {
     users.push({id:users.length,name});
     this.setState({users});
   }
+  addMessage(body){
+    let {messages, users} = this.state;
+    let createdAt = new Date;
+    let author = users.length > 0 ? users[0].name : 'anonymous';
+    messages.push({id: messages.length, body, createdAt, author});
+    this.setState({messages});
+  }
   render(){
     return(
       <div className='app'>
@@ -37,6 +47,10 @@ class App extends Component {
           addUser={this.addUser.bind(this)}
           />
         </div>
+        <MessageSection 
+        {...this.state}
+        addMessage = {this.addMessage.bind(this)}
+        />
       </div>
     );
   }
