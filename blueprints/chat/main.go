ed2a6c5d595e504739bab04c21efb4c27744bca8
Leland Krych <Leland.Krych@gmail.com>
2017-12-21 14:01:48 -0800

    make server listen to any port with cl input and template magik

--- blueprints/chat/main.go
+++ blueprints/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
@@ -21,18 +22,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	//get the room going
 	go r.run()
 	//start the web server
-	fmt.Println("Server listening on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Printf("Server listening on port %v \n", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
