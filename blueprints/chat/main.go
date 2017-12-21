9b5ee97d892c8ef10f1b8f365ec08fcb52bafa9f
Leland Krych <Leland.Krych@gmail.com>
2017-12-21 12:34:39 -0800

    use templates to generate html

--- blueprints/chat/main.go
+++ blueprints/chat/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-				<html>
-					<head>
-						<title>Chat</title>
-					</head>
-					<body>
-						Let us chat!
-					</body>
-				</html>`))
+//templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+//serveHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	//start the web server
+	fmt.Println("Serving on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
