4f6ceab0246ce6e63bf6f58da301f1346a137538
Leland Krych <Leland.Krych@gmail.com>
2017-12-21 13:19:43 -0800

    finish web server

--- blueprints/chat/main.go
+++ blueprints/chat/main.go
@@ -25,10 +25,17 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	//get the room going
+	go r.run()
 	//start the web server
-	fmt.Println("Serving on port 8080")
+	fmt.Println("Server listening on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("ListenAndServe:", err)
 	}
 }
+
+//room is run on separate goroutine so that the chatting operations occur in the background, allowing our main
+//goroutine to run the web server.
