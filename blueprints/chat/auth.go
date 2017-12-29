6fb74795e4cfd1311ebbe4bbb92166ecf0685a41
Leland Krych <Leland.Krych@gmail.com>
2017-12-29 13:34:20 -0700

    add login handler func that we can use to handle logging in with OAuth services

--- blueprints/chat/auth.go
+++ blueprints/chat/auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -30,4 +35,25 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // MustAuth creates an auth handler struct that wraps any other http handler
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+//loginHandler handles the third-party login process
+//format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	if len(segs) < 4 {
+		//code will panic if url is malformed
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "You must use a valid url. Ex: /auth/login/facebook")
+		return
+	}
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
