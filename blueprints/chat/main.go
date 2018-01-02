f149af5322ba0cb1e7223832e0ea0b09fd9685c7
Leland Krych <Leland.Krych@gmail.com>
2018-01-02 08:40:43 -0800

    add omniauth support to main file

--- blueprints/chat/main.go
+++ blueprints/chat/main.go
@@ -4,12 +4,18 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"trace"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/signature"
 )
 
 //templ represents a single template
@@ -17,6 +23,13 @@ type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
+}
+
+type config struct {
+	google_client string `yaml:"GOOGLE_CLIENT_ID"`
+	google_secret string `yaml:"GOOGLE_SECRET"`
+	github_client string `yaml:"GITHUB_CLIENT_ID"`
+	github_secret string `yaml:"GOOGLE_SECRET"`
 }
 
 //serveHTTP handles the HTTP request
@@ -30,6 +43,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse() // parse the flags
+	//parse secrets
+	var c config
+	c.getConfig()
+	//setup gomniauth
+	gomniauth.SetSecurityKey(signature.RandomKey(64))
+	gomniauth.WithProviders(
+		google.New(c.google_client, c.google_secret, "http://localhost:8080/auth/callback/google"),
+		github.New(c.github_client, c.github_secret, "http://localhost:8080/auth/callback/github"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
@@ -47,3 +69,15 @@ func main() {
 
 //room is run on separate goroutine so that the chatting operations occur in the background, allowing our main
 //goroutine to run the web server.
+
+func (c *config) getConfig() *config {
+	yaml, err := ioutil.ReadFile("secrets.yml")
+	if err != nil {
+		log.Printf("Error reading YAML: %v", err)
+	}
+	err = yaml.Unmarshal(yaml, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	return c
+}
