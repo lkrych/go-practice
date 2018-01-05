aa7d8ab3619d825e17c283ec13315188a4252eb9
Leland Krych <Leland.Krych@gmail.com>
2018-01-04 21:55:48 -0800

    create chatUser interface so that we can cache avatar information on the receipt of the first message instead of getting it every time a user sends a message

--- blueprints/chat/auth.go
+++ blueprints/chat/auth.go
@@ -4,14 +4,34 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/stretchr/objx"
 
 	"github.com/stretchr/gomniauth"
+
+	gomniauthcommon "github.com/stretchr/gomniauth/common"
 )
 
+//ChatUser exposes information needed in order for our Avatar implementations to generate correct URLs
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+
+//uses type embedding which means that our struct interface implements the common.User interface automatically
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
+
+//wraps handlers
 type authHandler struct {
 	next http.Handler
 }
@@ -85,14 +105,18 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("Error when trying to get user from %s: %s", provider, err), http.StatusInternalServerError)
 			return
 		}
+		chatUser := &chatUser{User: user}
 		m := md5.New()
 		io.WriteString(m, strings.ToLower(user.Email()))
-		userID := fmt.Sprintf("%x", m.Sum(nil))
+		chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+		avatarURL, err := avatars.GetAvatarURL(chatUser)
+		if err != nil {
+			log.Fatalln("Error when trying to GetAvatarURL", "-", err)
+		}
 		authCookieValue := objx.New(map[string]interface{}{
-			"userid":     userID,
+			"userid":     chatUser.uniqueID,
 			"name":       user.Name(),
-			"avatar_url": user.AvatarURL(),
-			"email":      user.Email(),
+			"avatar_url": avatarURL,
 		}).MustBase64()
 
 		http.SetCookie(w, &http.Cookie{
