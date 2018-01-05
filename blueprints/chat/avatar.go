ed35356b1caa09510057464e57bc07b67c471cb7
Leland Krych <Leland.Krych@gmail.com>
2018-01-05 07:45:00 -0800

    create helper function that allows you to use all three avatars implementations in build

--- blueprints/chat/avatar.go
+++ blueprints/chat/avatar.go
@@ -16,6 +16,17 @@ type Avatar interface {
 	GetAvatarURL(ChatUser) (string, error)
 }
 
+type TryAvatars []Avatar
+
+func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
+	for _, avatar := range a {
+		if url, err := avatar.GetAvatarURL(u); err == nil {
+			return url, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
 type AuthAvatar struct{}
 
 var UseAuthAvatar AuthAvatar // variable with AuthAvatar type but nil value, can later assign this variable to any field looking for an Avatar interface type
