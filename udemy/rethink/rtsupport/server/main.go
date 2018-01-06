055b9e60d3e716137118e83d22973e2daf6cd362
Leland Krych <Leland.Krych@gmail.com>
2018-01-06 13:32:11 -0800

    remove message struct from main

--- udemy/rethink/rtsupport/server/main.go
+++ udemy/rethink/rtsupport/server/main.go
@@ -6,12 +6,6 @@ import (
 	"net/http"
 )
 
-//Message is how json will be received in the server, we use an empty interface to accept any type of data
-type Message struct {
-	Name string      `json:"name"`
-	Data interface{} `json:"data"`
-}
-
 type Channel struct {
 	ID   string `json:"id"`
 	Name string `json:"data"`
