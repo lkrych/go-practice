0cca377a82d6e9c1d058a27d793f8b477779c9fa
Leland Krych <Leland.Krych@gmail.com>
2018-01-07 20:29:35 -0800

    Create the handlers for messages and users

--- udemy/rethink/rtsupport/server/handlers.go
+++ udemy/rethink/rtsupport/server/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/mitchellh/mapstructure"
 	r "gopkg.in/gorethink/gorethink.v4"
 )
@@ -62,4 +64,106 @@ func subscribeChannel(client *Client, data interface{}) {
 
 func unsubscribeChannel(client *Client, data interface{}) {
 	client.StopForKey(ChannelStop)
+}
+
+func editUser(client *Client, data interface{}) {
+	var user User
+	err := mapstructure.Decode(data, &user)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+	client.userName = user.Name
+	go func() {
+		_, err := r.Table("user").Get(client.id).Update(user).RunWrite(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
+
+func subscribeUser(client *Client, data interface{}) {
+	go func() {
+		stop := client.NewStopChannel(UserStop)
+		cursor, err := r.Table("user").Changes(r.ChangesOpts{IncludeInitial: true}).Run(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		changeFeedHelper(cursor, "user", client.send, stop)
+	}()
+}
+
+func unsubscribeUser(client *Client, data interface{}) {
+	client.StopForKey(UserStop)
+}
+
+func addChannelMessage(client *Client, data interface{}) {
+	var channelMessage ChannelMessage
+	err := mapstructure.Decode(data, &channelMessage)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+	}
+	go func() {
+		channelMessage.CreatedAt = time.Now()
+		channelMessage.Author = client.userName
+		err := r.Table("message").Insert(channelMessage).Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
+
+func subscribeChannelMessage(client *Client, data interface{}) {
+	go func() {
+		eventData := data.(map[string]interface{})
+		val, ok := eventData["channelID"]
+		if !ok {
+			return
+		}
+		channelID, ok := val.(string)
+		if !ok {
+			return
+		}
+		stop := client.NewStopChannel(MessageStop)
+		cursor, err := r.Table("message").OrderBy(r.OrderByOpts{Index: r.Desc("createdAt")}).
+			Filter(r.Row.Field("channelID")).Eq(channelID).
+			Changes(r.ChangesOpts{IncludeInitial: true}).
+			Run(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		changeFeedHelper(cursor, "message", client.send, stop)
+	}()
+}
+
+func unsubscribeChannelMessage(client *Client, data interface{}) {
+	client.StopForKey(MessageStop)
+}
+
+func changeFeedHelper(cursor *r.Cursor, changeEventName string, send chan<- Message, stop <-chan bool) {
+	change := make(chan r.ChangeResponse)
+	cursor.Listen(change)
+	for {
+		eventName := ""
+		var data interface{}
+		select {
+		case <-stop:
+			cursor.Close()
+			return
+		case val := <-change:
+			if val.NewValue != nil && val.OldValue == nil {
+				eventName = changeEventName + " add"
+				data = val.NewValue
+			} else if val.NewValue == nil && val.OldValue != nil {
+				eventName = changeEventName + " remove"
+				data = val.OldValue
+			} else if val.NewValue != nil && val.OldValue != nil {
+				eventName = changeEventName + " edit"
+				data = val.NewValue
+			}
+			send <- Message{eventName, data}
+		}
+	}
 }
