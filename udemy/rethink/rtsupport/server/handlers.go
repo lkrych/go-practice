46f1afeff74d75ccefe6251b0889efc901a60614
Leland Krych <Leland.Krych@gmail.com>
2018-01-07 19:49:58 -0800

    create subscribeCHannel handler

--- udemy/rethink/rtsupport/server/handlers.go
+++ udemy/rethink/rtsupport/server/handlers.go
@@ -5,6 +5,12 @@ import (
 	r "gopkg.in/gorethink/gorethink.v4"
 )
 
+const (
+	ChannelStop = iota
+	UserStop
+	MessageStop
+)
+
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
 	err := mapstructure.Decode(data, &channel)
@@ -20,4 +26,40 @@ func addChannel(client *Client, data interface{}) {
 		}
 	}()
 
+}
+
+func subscribeChannel(client *Client, data interface{}) {
+	stop := client.NewStopChannel(ChannelStop)
+	result := make(chan r.ChangeResponse)
+	cursor, err := r.Table("channel").Changes(r.ChangesOpts{IncludeInitial: true}).Run(client.session)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+	go func() {
+		var change r.ChangeResponse
+		for cursor.Next(&change) {
+			result <- change
+			if change.NewValue != nil && change.OldValue == nil {
+				client.send <- Message{"channel add", change.NewValue}
+			}
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case <-stop:
+				cursor.Close()
+				return
+			case change := <-result:
+				if change.NewValue != nil && change.OldValue == nil {
+					client.send <- Message{"channel add", change.NewValue}
+				}
+			}
+		}
+	}()
+}
+
+func unsubscribeChannel(client *Client, data interface{}) {
+	client.StopForKey(ChannelStop)
 }
