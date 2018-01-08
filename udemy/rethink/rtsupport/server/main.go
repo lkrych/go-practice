c9ad97c1502962c193fc84ee57dc16097bbef05b
Leland Krych <Leland.Krych@gmail.com>
2018-01-07 18:22:05 -0800

    setup db and import user andchannel structs

--- udemy/rethink/rtsupport/server/main.go
+++ udemy/rethink/rtsupport/server/main.go
@@ -4,21 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	r "gopkg.in/gorethink/gorethink.v4"
 )
 
 type Channel struct {
-	ID   string `json:"id"`
-	Name string `json:"data"`
+	ID   string `json:"id" gorethink:"id,omitempty"`
+	Name string `json:"data" gorethink:"name"`
+}
+
+type User struct {
+	ID   string `gorethink:"id,omitempty"`
+	Name string `gorethink:"name"`
 }
 
 func main() {
-	router := NewRouter()
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "rtsupport",
+	})
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	router := NewRouter(session)
 
 	router.Handle("channel add", addChannel)
 
 	http.Handle("/", router)
 	fmt.Printf("Server running on port 4000 \n")
-	err := http.ListenAndServe(":4000", nil)
+	err = http.ListenAndServe(":4000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
