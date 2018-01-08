b7cc318ef60ba3f0e4fe33999f4b63ec8cd100b4
Leland Krych <Leland.Krych@gmail.com>
2018-01-07 18:21:48 -0800

    insert channel into db with addCHannel handler

--- udemy/rethink/rtsupport/server/handlers.go
+++ udemy/rethink/rtsupport/server/handlers.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/mitchellh/mapstructure"
+	r "gopkg.in/gorethink/gorethink.v4"
 )
 
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
-	var message Message
-	mapstructure.Decode(data, &channel)
-	fmt.Printf("%#v\n", channel)
-	//insert new channel into database
-	channel.ID = "ABC123"
-	message.Name = "channel add"
-	message.Data = channel
-	client.send <- message
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+	//insert new channel into database using goroutine to prevent blocking
+	go func() {
+		err = r.Table("channel").Insert(channel).Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+
 }
