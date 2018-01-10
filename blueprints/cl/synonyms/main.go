5666399c1af66ae286ee103e2db9d864acca7a14
Leland Krych <Leland.Krych@gmail.com>
2018-01-10 07:45:19 -0800

    add main for thesaurus

---
+++ blueprints/cl/synonyms/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"cl/thesaurus"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	//parse secrets
+	var thesaurus := &thesaurus.Config{}
+	thesaurus.GetConfig()
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			log.Fatalln("Failed when looking for synonyms for %v: %v", word, err)
+		}
+		if len(syns) == 0 {
+			log.Fatalln("Couldn't find synonyms for %v", word)
+		}
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+
+}
