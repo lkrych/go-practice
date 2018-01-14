a55eceaf9afec5411e18e35299175f3ef4203cb9
Leland Krych <Leland.Krych@gmail.com>
2018-01-13 18:07:57 -0800

    add database helper functions and poll struct

--- blueprints/socialpoll/twittervotes/main.go
+++ blueprints/socialpoll/twittervotes/main.go
@@ -1,3 +1,38 @@
 package main
 
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) { //iter is an mgo object that allows us to access each poll one by one.
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("closed database connection")
+}
+
 func main() {}
