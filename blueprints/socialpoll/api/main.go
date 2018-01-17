3cbc222d53bcf53d578b9296f31033a3af01d7cf
Leland Krych <Leland.Krych@gmail.com>
2018-01-17 08:33:23 -0800

    add cors handler and add apikey handler

--- blueprints/socialpoll/api/main.go
+++ blueprints/socialpoll/api/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "context"
+import (
+	"context"
+	"net/http"
+)
 
 type contextKey struct {
 	name string
@@ -14,4 +17,29 @@ func main() {
 func APIKey(ctx context.Context) (string, bool) {
 	key, ok := ctx.Value(contextKeyAPIKey).(string)
 	return key, ok
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func withAPIKEY(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if !isValidAPIKey(key) {
+			respondErr(w, r, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+		ctx := context.WithValue(r.Context(), contextKeyAPIKey, key)
+		fn(w, r.WithContext(ctx))
+	}
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//allow service to websites on other domains
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
 }
