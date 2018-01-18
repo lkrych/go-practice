b72f53f0e0354521a9c287f6358c9de905da847c
Leland Krych <Leland.Krych@gmail.com>
2018-01-18 08:47:12 -0800

    Add handler for DELETE

--- blueprints/socialpoll/api/polls.go
+++ blueprints/socialpoll/api/polls.go
@@ -54,7 +54,25 @@ func (s *Server) handlePollsGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) handlePollsPost(w http.ResponseWriter, r *http.Request) {
-	respondErr(w, r, http.StatusInternalServerError, errors.New("not implemented"))
+	session := s.db.Copy()
+	defer session.Close()
+	c := session.DB("ballots").C("polls")
+	var p poll
+	if err := decodeBody(r, &p); err != nil {
+		respondErr(w, r, http.StatusBadRequest, "failed to read poll from request", err)
+		return
+	}
+	apikey, ok := APIKey(r.Context())
+	if ok {
+		p.APIKey = apikey
+	}
+	p.ID = bson.NewObjectId()
+	if err := c.Insert(p); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, "failed to insert poll", err)
+		return
+	}
+	w.Header().Set("Location", "polls/"+p.ID.Hex())
+	respond(w, r, http.StatusCreated, nil)
 }
 
 func (s *Server) handlePollsDelete(w http.ResponseWriter, r *http.Request) {
