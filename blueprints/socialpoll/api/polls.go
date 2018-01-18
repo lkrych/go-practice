ea98352685fe67d3de462fd71cba7656e9780bf9
Leland Krych <Leland.Krych@gmail.com>
2018-01-18 08:40:04 -0800

    create handler function for GET request, stub out POST and DELETE

---
+++ blueprints/socialpoll/api/polls.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type poll struct {
+	ID      bson.ObjectId  `bson:"_id" json:"id"`
+	Title   string         `json:"title"`
+	Options []string       `json:"options"`
+	Results map[string]int `json:"results,omitempty"`
+	APIKey  string         `json:"apikey"`
+}
+
+func (s *Server) handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		s.handlePollsGet(w, r)
+		return
+	case "POST":
+		s.handlePollsPost(w, r)
+		return
+	case "DELETE":
+		s.handlePollsDelete(w, r)
+		return
+	}
+	//not found
+	respondHTTPErr(w, r, http.StatusNotFound)
+}
+
+func (s *Server) handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	session := s.db.Copy()
+	defer session.Close()
+	c := session.DB("ballots").C("polls")
+	var q *mgo.Query
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		//get specific polll
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		//get all polls
+		q = c.Find(nil)
+	}
+	var result []*poll
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+	respond(w, r, http.StatusOK, &result)
+}
+
+func (s *Server) handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("not implemented"))
+}
+
+func (s *Server) handlePollsDelete(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("not implemented"))
+}
