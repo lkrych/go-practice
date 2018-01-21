8cbf7cf2d5d65e7df296151dabf62011203b49c9
Leland Krych <Leland.Krych@gmail.com>
2018-01-21 11:34:55 -0800

    import API key into main file

--- blueprints/meander/cmd/meander/main.go
+++ blueprints/meander/cmd/meander/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"meander"
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
-	//meander.APIKey = "TODO"
+	APIKey := &APIKeys{}
+	APIKey = APIKey.readKeys()
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
 	})
@@ -22,4 +26,22 @@ func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
 		publicData[i] = meander.Public(d)
 	}
 	return json.NewEncoder(w).Encode(publicData)
+}
+
+type APIKeys struct {
+	ConsumerKey string `yaml:"GOOGLE_PLACES_SECRET"`
+}
+
+func (k *APIKeys) readKeys() *APIKeys {
+	yamlFile, err := ioutil.ReadFile("/Users/Leland/go/go_code/blueprints/meander/api_keys/secrets.yml")
+
+	if err != nil {
+		log.Printf("Error reading YAML: %v", err)
+	}
+	err = yaml.Unmarshal(yamlFile, k)
+
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	return k
 }
