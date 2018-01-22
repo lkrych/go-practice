720b31afef3b800efa966df4d88662c1d0975eae
Leland Krych <Leland.Krych@gmail.com>
2018-01-22 08:34:22 -0800

    add recommendations handler function to main

--- blueprints/meander/cmd/meander/main.go
+++ blueprints/meander/cmd/meander/main.go
@@ -5,12 +5,37 @@ import (
 	"log"
 	"meander"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
 	})
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|")}
+		var err error
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
 	log.Println("Starting web server on 8080")
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
