6f0cfbdf5b841a5cdbbb3f07bd0d6f6c1636acc3
Leland Krych <Leland.Krych@gmail.com>
2018-01-26 08:23:10 -0800

    hookup cl tool with filedb

---
+++ blueprints/backup/cmds/backup/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+)
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func (p path) String() string {
+	return fmt.Sprintf("%s [%s]", p.Path, p.Hash)
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		dbpath = flag.String("db", "./backupdata", "path to database directory")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("invalid usage; must specify command")
+		return
+	}
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+	switch strings.ToLower(args[0]) {
+	case "list":
+		var path path
+		//the callback function expects a bool
+		//returning true stops the iteration
+		col.ForEach(func(i int, data []byte) bool {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return true
+			}
+			fmt.Printf("= %s\n", path)
+			return false
+		})
+	case "add":
+		if len(args[1:]) == 0 {
+			fatalErr = errors.New("must specify path to add")
+			return
+		}
+		//by default we'll set the hash to "Not yet archived"
+		//this is an invalid hash but serves the dual purpose of letting the user
+		//know that it hasn't been archived as well as indicating this in our code
+		for _, p := range args[1:] {
+			path := &path{Path: p, Hash: "Not yet archived"}
+			if err := col.InsertJSON(path); err != nil {
+				fatalErr = err
+				return
+			}
+			fmt.Printf("+ %s\n", path)
+		}
+	case "remove":
+		var path path
+		//the callback function we provide expects two bools,
+		//the first to indicate whether the item should be removed
+		//the second to indicate whether we should stop iterating
+		col.RemoveEach(func(i int, data []byte) (bool, bool) {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return false, true
+			}
+			for _, p := range args[1:] {
+				if path.Path == p {
+					fmt.Printf("- %s\n", path)
+					return true, false
+				}
+			}
+			return false, false
+		})
+	}
+}
