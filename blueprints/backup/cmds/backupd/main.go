c541c8358d6272e6cf2f357f89b12f3b9d6a022c
Leland Krych <Leland.Krych@gmail.com>
2018-01-26 08:46:03 -0800

    begin construction of backup daemon

---
+++ blueprints/backup/cmds/backupd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"backup"
+	"flag"
+	"log"
+	"time"
+
+	"github.com/matryer/filedb"
+)
+
+//this daemon will be responsible for periodically checking the
+//paths listed in the filedb database, hashing the folder to see
+//whether anything has changed, and using the backup package to
+//perform the archiving of the folders that need it
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		interval = flag.Duration("interval", 10*time.Second, "interval between checks")
+		archive  = flag.String("archive", "archive", "path to archive location")
+		dbpath   = flag.String("db", "./db", "path to filedb database")
+	)
+	flag.Parse()
+	m := &backup.Monitor{
+		Destination: *archive,
+		Archiver:    backup.ZIP,
+		Paths:       make(map[string]string),
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+}
