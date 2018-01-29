4f81e11350e5224a78c58c06c61e0759ab517e28
Leland Krych <Leland.Krych@gmail.com>
2018-01-29 08:28:20 -0800

    make sure backup daemon can observe and update the db if files are changed

--- blueprints/backup/cmds/backupd/main.go
+++ blueprints/backup/cmds/backupd/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"backup"
+	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/matryer/filedb"
@@ -21,6 +27,7 @@ type path struct {
 
 func main() {
 	var fatalErr error
+	var path path
 	defer func() {
 		if fatalErr != nil {
 			log.Fatalln(fatalErr)
@@ -48,5 +55,64 @@ func main() {
 	if err != nil {
 		fatalErr = err
 		return
+	}
+	//eager-load data from db in Paths map
+	col.ForEach(func(_ int, data []byte) bool {
+		if err := json.Unmarshal(data, &path); err != nil {
+			fatalErr = err
+			return true
+		}
+		m.Paths[path.Path] = path.Hash
+		return false //continue
+	})
+	if fatalErr != nil {
+		return
+	}
+	if len(m.Paths) < 1 {
+		fatalErr = errors.New("no paths - use backup tool to add at least one")
+		return
+	}
+	//set up signal channels
+	check(m, col)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case <-time.After(*interval):
+			check(m, col)
+		case <-signalChan:
+			//stop
+			fmt.Println()
+			log.Printf("Stopping backup daemon...")
+			return
+		}
+	}
+}
+
+func check(m *backup.Monitor, col *filedb.C) {
+	log.Println("Checking...")
+	counter, err := m.Now()
+	if err != nil {
+		log.Fatalln("failed to backup:", err)
+	}
+	if counter > 0 {
+		log.Printf(" Archived %d directories\n", counter)
+		//update hashes
+		var path path
+		col.SelectEach(func(_ int, data []byte) (bool, []byte, bool) {
+			if err := json.Unmarshal(data, &path); err != nil {
+				log.Println("failed to unmarshal data (skipping):", err)
+				return true, data, false
+			}
+			path.Hash, _ = m.Paths[path.Path]
+			newdata, err := json.Marshal(&path)
+			if err != nil {
+				log.Println("failed to marshal data (skipping):", err)
+				return true, data, false
+			}
+			return true, newdata, false
+		})
+	} else {
+		log.Println(" No changes detected!")
 	}
 }
