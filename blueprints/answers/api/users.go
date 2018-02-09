154a9be8ff2500fc691d899fdf269a6aa8da722e
Leland Krych <Leland.Krych@gmail.com>
2018-02-09 08:37:22 -0800

    finish the code the loads the user

--- blueprints/answers/api/users.go
+++ blueprints/answers/api/users.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/user"
 )
 
@@ -19,8 +20,26 @@ type User struct {
 func UserFromAEUser(ctx context.Context) (*User, error) {
 	aeuser := user.Current(ctx)
 	if aeuser == nil {
-		return nil, errors.New("not logged in")
+		return nil, errors.New("Not logged in")
 	}
 	var appUser User
 	appUser.Key = datastore.NewKey(ctx, "User", aeuser.ID, 0, nil)
+	err := datastore.Get(ctx, appUser.Key, &appUser)
+	if err != nil && err != datastore.ErrNoSuchEntity {
+		return nil, err
+	}
+	if err == nil {
+		return &appUser, nil
+	}
+	//this only runs if this is the first time the user has logged in
+	//the return value from datastore.Get will be ErrNoSuchEntity
+	appUser.UserID = aeuser.ID
+	appUser.DisplayName = aeuser.String()
+	appUser.AvatarURL = gravatarURL(aeuser.Email)
+	log.Infof(ctx, "Saving new user: %s", aeuser.String())
+	appUser.Key, err = datastore.Put(ctx, appUser.Key, &appUser)
+	if err != nil {
+		return nil, err
+	}
+	return &appUser, nil
 }
