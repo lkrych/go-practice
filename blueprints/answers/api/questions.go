aec5bd21c63e16382724b43cfbd7b2bdd4e54937
Leland Krych <Leland.Krych@gmail.com>
2018-02-09 08:13:50 -0800

    Add documentation and GetQuestion helper

--- blueprints/answers/api/questions.go
+++ blueprints/answers/api/questions.go
@@ -1,3 +1,5 @@
+package api
+
 import (
 	"context"
 	"errors"
@@ -14,6 +16,7 @@ type Question struct {
 	AnswersCount int            `json: "answers_count"`
 }
 
+//OK checks to make sure that question length is valid
 func (q Question) OK() error {
 	if len(q.Question) < 10 {
 		return errors.New("question is too short")
@@ -21,6 +24,7 @@ func (q Question) OK() error {
 	return nil
 }
 
+//Create builds a Question in the Datastore (a NoSQL db)
 func (q *Question) Create(ctx context.Context) error {
 	log.Debugf(ctx, "Saving question: %s", q.Question)
 	if q.Key == nil {
@@ -39,6 +43,7 @@ func (q *Question) Create(ctx context.Context) error {
 	return nil
 }
 
+//Update updates a question in the Datastore
 func (q *Question) Update(ctx context.Context) error {
 	if q.Key == nil {
 		q.Key = datastore.NewIncompleteKey(ctx, "Question", nil)
@@ -49,4 +54,15 @@ func (q *Question) Update(ctx context.Context) error {
 		return err
 	}
 	return nil
+}
+
+//GetQuestion returns a Question struct with the key specified in the argument
+func GetQuestion(ctx context.Context, key *datastore.Key) (*Question, error) {
+	var q Question
+	err := datastore.Get(ctx, key, &q)
+	if err != nil {
+		return nil, err
+	}
+	q.Key = key
+	return &q, nil
 }
