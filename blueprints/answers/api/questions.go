93c67bb4e08695dbbf5f9172e0be1f85f7f268ab
Leland Krych <Leland.Krych@gmail.com>
2018-02-09 08:07:27 -0800

    create question struct and helper methods for creating and checking if a question is valid

---
+++ blueprints/answers/api/questions.go
@@ -0,0 +1,40 @@
+import (
+	"context"
+	"errors"
+	"time"
+
+	datastore "google.golang.org/genproto/googleapis/datastore/v1"
+)
+
+type Question struct {
+	Key          *datastore.Key `json: "id" datastore: "-"`
+	CTime        time.Time      `json: "created"`
+	Question     string         `json: "question"`
+	User         UserCard       `json: "user"`
+	AnswersCount int            `json: "answers_count"`
+}
+
+func (q Question) OK() error {
+	if len(q.Question) < 10 {
+		return errors.New("question is too short")
+	}
+	return nil
+}
+
+func (q *Question) Create(ctx context.Context) error {
+	log.Debugf(ctx, "Saving question: %s", q.Question)
+	if q.Key == nil {
+		q.Key = datastore.NewIncompleteKey(ctx, "Question", nil)
+	}
+	user, err := UserFromAEUser(ctx)
+	if err != nil {
+		return err
+	}
+	q.User = user.Card()
+	q.CTime = time.Now()
+	q.Key, err = datastore.Put(ctx, q.Key, q)
+	if err != nil {
+		return err
+	}
+	return nil
+}
