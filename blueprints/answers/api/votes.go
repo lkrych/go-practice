ce44b4eb8074225dcaf3b0813d9a98a6213a498b
Leland Krych <Leland.Krych@gmail.com>
2018-02-13 08:52:53 -0800

    create CastVote function

--- blueprints/answers/api/votes.go
+++ blueprints/answers/api/votes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"time"
 
 	"google.golang.org/appengine/datastore"
@@ -13,4 +14,28 @@ type Vote struct {
 	Answer   AnswerCard     `json:"answer" datastore:",noindex"`
 	User     UserCard       `json:"user" datastore:",noindex"`
 	Score    int            `json:"score" datastore:",noindex"`
+}
+
+func CastVote(ctx context.Context, answerKey *datastore.Key, score int) (*Vote, error) {
+	question, err := GetQuestion(ctx, answerKey.Parent())
+	if err != nil {
+		return nil, err
+	}
+	user, err := UserFromAEUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var vote Vote
+	err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+		var err error
+		vote, err = castVoteInTransaction(ctx, answerKey, question, user, score)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, &datastore.TransactionOptions{XG: true})
+	if err != nil {
+		return nil, err
+	}
+	return &vote, nil
 }
