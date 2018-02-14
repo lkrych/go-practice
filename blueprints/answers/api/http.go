d5a893275d4b8ff0613dcc6021b2c866bb37a825
Leland Krych <Leland.Krych@gmail.com>
2018-02-14 08:36:46 -0800

    Add a helper to parse path params

--- blueprints/answers/api/http.go
+++ blueprints/answers/api/http.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"google.golang.org/appengine/log"
 )
@@ -53,4 +54,17 @@ func respondErr(ctx context.Context, w http.ResponseWriter, r *http.Request, err
 	if err != nil {
 		log.Errorf(ctx, "respondErr: %s", err)
 	}
+}
+
+func pathParams(r *http.Request, pattern string) map[string]string {
+	params := map[string]string{}
+	pathSegs := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+
+	for i, seg := range strings.Split(strings.Trim(pattern, "/"), "/") {
+		if i > len(pathSegs)-1 {
+			return params
+		}
+		params[seg] = pathSegs[i]
+	}
+	return params
 }
