dfb6b64e553112e73a58ac44e222835f01bb3aa8
Leland Krych <Leland.Krych@gmail.com>
2018-02-15 08:18:51 -0800

    fetch answers using url query params

--- blueprints/answers/api/handle_answers.go
+++ blueprints/answers/api/handle_answers.go
@@ -1,6 +1,11 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+)
 
 func handleAnswers(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -11,4 +16,24 @@ func handleAnswers(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.NotFound(w, r)
 	}
+}
+
+//handleAnswersGet uses url query parameters, not path parameters, this is
+//to demonstrate different styles of api design, ordinarily you should be
+//consistent
+func handleAnswersGet(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	q := r.URL.Query()
+	questionIDStr := q.Get("question_id")
+	questionKey, err := datastore.DecodeKey(questionIDStr)
+	if err != nil {
+		respondErr(ctx, w, r, err, http.StatusBadRequest)
+		return
+	}
+	answers, err := GetAnswers(ctx, questionKey)
+	if err != nil {
+		respondErr(ctx, w, r, err, http.StatusInternalServerError)
+		return
+	}
+	respond(ctx, w, r, answers, http.StatusOK)
 }
