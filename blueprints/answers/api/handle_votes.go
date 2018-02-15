c1fe43dee1080ef969d4fe8559968a714cd07532
Leland Krych <Leland.Krych@gmail.com>
2018-02-15 08:42:53 -0800

    add http handler for votes

--- blueprints/answers/api/handle_votes.go
+++ blueprints/answers/api/handle_votes.go
@@ -1,6 +1,12 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+)
 
 func handleVotes(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -8,4 +14,38 @@ func handleVotes(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	handleVote(w, r)
+}
+
+func handleVote(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	var newVote struct {
+		AnswerID string `json:"answer_id"`
+		Score    int    `json:"score"`
+	}
+
+	err := decode(r, &newVote)
+	if err != nil {
+		respondErr(ctx, w, r, err, http.StatusBadRequest)
+		return
+	}
+
+	err = validScore(newVote.Score)
+	if err != nil {
+		respondErr(ctx, w, r, err, http.StatusBadRequest)
+		return
+	}
+
+	answerKey, err := datastore.DecodeKey(newVote.AnswerID)
+	if err != nil {
+		respondErr(ctx, w, r, errors.New("invalid answer_id"), http.StatusBadRequest)
+		return
+	}
+
+	vote, err := CastVote(ctx, answerKey, newVote.Score)
+	if err != nil {
+		respondErr(ctx, w, r, err, http.StatusInternalServerError)
+		return
+	}
+
+	respond(ctx, w, r, vote, http.StatusCreated)
 }
