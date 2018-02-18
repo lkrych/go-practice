7c949f7b6d544c884e513c7a83ffa9115c9252c4
Leland Krych <Leland.Krych@gmail.com>
2018-02-18 09:18:28 -0800

    define the Service interface

---
+++ blueprints/vault/service.go
@@ -0,0 +1,12 @@
+package vault
+
+import "golang.org/x/net/context"
+
+// Service provides password hashing capabilities
+
+type Service interface {
+	Hash(ctx context.Context, password string) (string, error)
+	Validate(ctx context.Context, password string, hash string) (bool, error)
+}
+
+type vaultService struct{}
