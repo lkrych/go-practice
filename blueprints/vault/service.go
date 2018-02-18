e8a78a674fa069086d61fcc66d9cd7e7d78ac978
Leland Krych <Leland.Krych@gmail.com>
2018-02-18 09:52:17 -0800

    add encoding helper function

--- blueprints/vault/service.go
+++ blueprints/vault/service.go
@@ -76,3 +76,7 @@ func decodeValidateRequest(ctx context.Context, r *http.Request) (interface{}, e
 	}
 	return req, nil
 }
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
