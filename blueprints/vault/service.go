2b7399e07fc546b08080c75c2f857356d4450409
Leland Krych <Leland.Krych@gmail.com>
2018-02-18 09:30:36 -0800

    expose service interface using constructor

--- blueprints/vault/service.go
+++ blueprints/vault/service.go
@@ -3,10 +3,15 @@ package vault
 import "golang.org/x/net/context"
 
 // Service provides password hashing capabilities
-
 type Service interface {
 	Hash(ctx context.Context, password string) (string, error)
 	Validate(ctx context.Context, password string, hash string) (bool, error)
 }
 
 type vaultService struct{}
+
+//NewService makes a new Service. This prevents us from exposing our internals
+//and lets us do more work to prepare vaultService without changing the API
+func NewService() Service {
+	return vaultService{}
+}
