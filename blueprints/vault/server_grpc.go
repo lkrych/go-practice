e936d71af3fc667370b6a9438fe33f30e6933229
Leland Krych <Leland.Krych@gmail.com>
2018-02-19 07:58:03 -0800

    add methods for translating between protocol-buffer objects and internal implementation objects

--- blueprints/vault/server_grpc.go
+++ blueprints/vault/server_grpc.go
@@ -31,3 +31,45 @@ func (s *grpcServer) Validate(ctx context.Context, r *pb.ValidateRequest) (*Vali
 	}
 	return nil, rep.(*pb.ValidateResponse), nil
 }
+
+//We are going to need methods to translate between the structs we created in structure.go
+
+func EncodeGRPCHashRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(hashRequest)
+	return &pb.HashRequest{Password: req.Password}, nil
+}
+
+func DecodeGRPCHashRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(*pb.HashRequest)
+	return hashRequest{Password: req.Password}, nil
+}
+
+func EncodeGRPCHashResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(hashResponse)
+	return &pb.HashResponse{Hash: res.Hash, Err: res.Err}, nil
+}
+
+func DecodeGRPCHashResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(*pb.HashResponse)
+	return hashResponse{Hash: res.Hash, Err: res.Err}, nil
+}
+
+func EncodeGRPCValidateRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(validateRequest)
+	return &pb.ValidateRequest{Password: req.Password, Hash: req.Hash}, nil
+}
+
+func DecodeGRPCValidateRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(*pb.ValidateRequest)
+	return validateRequest{Password: req.Password, Hash: req.Hash}, nil
+}
+
+func EncodeGRPCValidateResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(validateResponse)
+	return &pb.ValidateResponse{Valid: res.Valid, Err: res.Err}, nil
+}
+
+func DecodeGRPCValidateResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(*pb.ValidateResponse)
+	return validateResponse{Valid: res.Valid}, nil
+}
