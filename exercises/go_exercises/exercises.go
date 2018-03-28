1576948a4923430f1d7063bf62e67bf1372fea1d
Leland Krych <Leland.Krych@gmail.com>
2018-03-28 07:58:28 -0700

    add new exercises

--- exercises/go_exercises/exercises.go
+++ exercises/go_exercises/exercises.go
@@ -1,24 +1,20 @@
 package goExercises
  
-//Return the indices of the elements in the arr that sum to find
-func twoSum(arr []int, find int) []int {
+//return sorted arr using quicksort
+func mergeSort(arr []int) []int {
 	
 }
-
-//create a more optimized version of the function
-func twoSumOptimized(arr []int, find int) []int {
-
-}
-
-//observe the benchmark code in the tests. Try to write this code after looking at it
  
 //return the longest word in sentence
 func longestWord(sentence string) string {
 
 }
  
-//return num of vowels in sentence
-func countVowels(sentence string) int {
-	
+//add dashes around an odd number, except on first or last el. Only allow one dash
+//ex: 303 => 3-0-3
+//ex: 333 => 3-3-3
+//ex: 3223 => 3-22-3
+func dasherizeNumber(n int) string {
+
 }
  
