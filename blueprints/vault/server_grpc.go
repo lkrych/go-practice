cfd0fac7ec2a5c4cd231f1ad456dd42032d241d9
Leland Krych <Leland.Krych@gmail.com>
2018-03-31 19:06:35 -0700

    add translation methods between protobus and structs, also create grpcServer constructor

--- blueprints/vault/server_grpc.go
+++ blueprints/vault/server_grpc.go
@@ -5,6 +5,7 @@ import (
 	"vault/pb"
 
 	grpctransport "github.com/go-kit/kit/transport/grpc"
+	xnetcontext "golang.org/x/net/context"
 )
 
 //In this file we are creating an interface for the service endpoints
@@ -15,7 +16,22 @@ type grpcServer struct {
 	validate grpctransport.Handler
 }
 
-func (s *grpcServer) Hash(ctx context.Context, r *pb.HashRequest) (*pb.HashResponse, error) {
+func NewGRPCServer(ctx context.Context, endpoints Endpoints) pb.VaultServer {
+	return &grpcServer{
+		hash: grpctransport.NewServer(
+			endpoints.HashEndpoint,
+			DecodeGRPCHashRequest,
+			EncodeGRPCHashResponse,
+		),
+		validate: grpctransport.NewServer(
+			endpoints.ValidateEndpoint,
+			DecodeGRPCValidateRequest,
+			EncodeGRPCValidateResponse,
+		),
+	}
+}
+
+func (s *grpcServer) Hash(ctx xnetcontext.Context, r *pb.HashRequest) (*pb.HashResponse, error) {
 	_, resp, err := s.hash.ServeGRPC(ctx, r)
 	if err != nil {
 		return nil, err
@@ -23,10 +39,61 @@ func (s *grpcServer) Hash(ctx context.Context, r *pb.HashRequest) (*pb.HashRespo
 	return resp.(*pb.HashResponse), nil
 }
 
-func (s *grpcServer) Validate(ctx context.Context, r *pb.ValidateRequest) (*pb.ValidateResponse, error) {
+func (s *grpcServer) Validate(ctx xnetcontext.Context, r *pb.ValidateRequest) (*pb.ValidateResponse, error) {
 	_, resp, err := s.validate.ServeGRPC(ctx, r)
 	if err != nil {
 		return nil, err
 	}
 	return resp.(*pb.ValidateResponse), nil
+}
+
+//we are using the request and response objects from the pb package, but our own endpoints use the structs we defined in service.go
+//we need to translate between the two.
+
+//EncodeGRPCHashRequest is used to translate our own hashRequest type into a protocol buffer type that can be used to communicated with the client.
+func EncodeGRPCHashRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(hashRequest)
+	return &pb.HashRequest{Password: req.Password}, nil
+}
+
+//DecodeGRPCHashRequest is used to translate our protocol buffer type into our hashRequest type that can be used to communicated with the client.
+func DecodeGRPCHashRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(*pb.HashRequest)
+	return hashRequest{Password: req.Password}, nil
+}
+
+//EncodeGRPCHashResponse is used to translate our own hashResponse type into a protocol buffer type that can be used to communicated with the client.
+func EncodeGRPCHashResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(hashResponse)
+	return &pb.HashResponse{Hash: res.Hash, Err: res.Err}, nil
+}
+
+//DecodeGRPCHashResponse is used to translate our protocol buffer type into our hashResponse type that can be used to communicated with the client.
+func DecodeGRPCHashResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(*pb.HashResponse)
+	return hashResponse{Hash: res.Hash, Err: res.Err}, nil
+}
+
+//EncodeGRPCValidateRequest is used to translate our own validateRequest type into a protocol buffer type that can be used to communicated with the client.
+func EncodeGRPCValidateRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(validateRequest)
+	return &pb.ValidateRequest{Password: req.Password, Hash: req.Hash}, nil
+}
+
+//DecodeGRPCValidateRequest is used to translate our protocol buffer type into our validateRequest type that can be used to communicated with the client.
+func DecodeGRPCValidateRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(*pb.ValidateRequest)
+	return validateRequest{Password: req.Password, Hash: req.Hash}, nil
+}
+
+//EncodeGRPCValidateResponse is used to translate our own validateResponse type into a protocol buffer type that can be used to communicated with the client.
+func EncodeGRPCValidateResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(validateResponse)
+	return &pb.ValidateResponse{Valid: res.Valid}, nil
+}
+
+//DecodeGRPCValidateResponse is used to translate our protocol buffer type into our validateResponse type that can be used to communicated with the client.
+func DecodeGRPCValidateResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(*pb.ValidateResponse)
+	return validateResponse{Valid: res.Valid}, nil
 }
