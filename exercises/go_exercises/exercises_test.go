9729574f44fd396268a4414c3a4c01e8065ada03
Leland Krych <Leland.Krych@gmail.com>
2018-04-10 19:21:48 -0700

    add exercise tests

--- exercises/go_exercises/exercises_test.go
+++ exercises/go_exercises/exercises_test.go
@@ -1,29 +1,82 @@
 package goExercises
- 
+
 import (
-"fmt"
-"testing"
-"github.com/google/go-cmp/cmp"
+	"testing"
 )
- 
-func TestRegister(t *testing.T) {
-	currMax1, max1 := register("./advent_input/register_beginner_input.txt")
-	currMax2, max2 := register("./advent_input/register_input.txt")
 
-	if currMax1 != 1 {
-		t.Errorf("Expected currentMax of test 1 to be 1, not %v", currMax1)
+func TestCheckSum(t *testing.T) {
+	test1 := checkSum([][]int{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}})
+	test2 := checkSum([][]int{{1, 2, 3, 4}, {1, 2, 3}, {1, 2, 3}})
+	test3 := checkSum([][]int{{1, 2, 3, 10}, {1, 2, 3, 12}, {1, 2, 3, 15}})
+	test4 := checkSum([][]int{{3, 22, 124}, {3, 70}, {1, 2, 5}})
+	test5 := checkSum([][]int{{1, 2, 10}, {1, 11, 3}, {500, 2, 3}})
+	test6 := checkSumAdvent()
+
+	if test1 != 6 {
+		t.Errorf("Expected answer to be 6, not %v", test1)
+	}
+	if test2 != 7 {
+		t.Errorf("Expected answer to be 7, not %v", test2)
 	}
 
-	if max1 != 10 {
-		t.Errorf("Expected max of test 1 to be 10, not %v", max1)
+	if test3 != 34 {
+		t.Errorf("Expected answer to be 34, not %v", test3)
 	}
 
-	if currMax2 != 4888 {
-		t.Errorf("Expected currentMax of test 1 to be 4888, not %v", currMax2)
+	if test4 != 192 {
+		t.Errorf("Expected answer to be 192, not %v", test4)
 	}
 
-	if max2 != 7774 {
-		t.Errorf("Expected max of test 1 to be 7774, not %v", max2)
+	if test5 != 517 {
+		t.Errorf("Expected answer to be 517, not %v", test5)
+	}
+
+	if test6 != 53978 {
+		t.Errorf("Expected answer to be 53978, not %v", test6)
 	}
 }
- 
+
+func TestIsPalindrome(t *testing.T) {
+	test1 := isPalindrome("abc")
+	test2 := isPalindrome("racecar")
+	test3 := isPalindrome("z")
+
+	if test1 != false {
+		t.Errorf("Expected isPalindrome of abc to be false, not %v", test1)
+	}
+
+	if test2 != true {
+		t.Errorf("Expected isPalindrome of racecar to be true, not %v", test2)
+	}
+
+	if test3 != true {
+		t.Errorf("Expected isPalindrome of z to be true, not %v", test3)
+	}
+}
+
+func TestBinarySearch(t *testing.T) {
+	test1 := binarySearch([]int{0, 1, 2, 3, 4, 5}, 3)
+	test2 := binarySearch([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 9)
+	test3 := binarySearch([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0)
+	test4 := binarySearch([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 10)
+	test5 := binarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0)
+
+	if test1 != 3 {
+		t.Errorf("Expected binarySearch to find the index of 3 at 4, not %v", test1)
+	}
+	if test2 != 9 {
+		t.Errorf("Expected binarySearch to find the index of 9 at 10, not %v", test2)
+	}
+
+	if test3 != 0 {
+		t.Errorf("Expected binarySearch to find the index of 0 at 0, not %v", test3)
+	}
+
+	if test4 != -1 {
+		t.Errorf("Expected binarySearch to find the index of 10 at -1, not %v", test4)
+	}
+
+	if test5 != -1 {
+		t.Errorf("Expected binarySearch to find the index of 0 at -1, not %v", test5)
+	}
+}
