8e187b09a956bbfd9408ee08f753f3712949099c
Leland Krych <Leland.Krych@gmail.com>
2018-04-18 16:10:03 -0700

    comment out hello_cli.go

--- in_practice/2_a_solid_foundation/hello_cli.go
+++ in_practice/2_a_solid_foundation/hello_cli.go
@@ -1,32 +1,32 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"os"
+// import (
+// 	"fmt"
+// 	"os"
 
-	"github.com/urfave/cli"
-)
+// 	"github.com/urfave/cli"
+// )
 
-func main() {
-	//create a new application
-	app := cli.NewApp()
-	app.Name = "hello_cli"
-	app.Usage = "Print hello world"
-	//setup global flag
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name: "name, n",
+// func main() {
+// 	//create a new application
+// 	app := cli.NewApp()
+// 	app.Name = "hello_cli"
+// 	app.Usage = "Print hello world"
+// 	//setup global flag
+// 	app.Flags = []cli.Flag{
+// 		cli.StringFlag{
+// 			Name: "name, n",
 
-			Value: "World",
-			Usage: "Who to say hello to.",
-		},
-	}
-	//define the action to run
-	app.Action = func(c *cli.Context) error {
-		name := c.GlobalString("name")
-		fmt.Printf("Hello %s!\n", name)
-		return nil
-	}
-	//run the application
-	app.Run(os.Args)
-}
+// 			Value: "World",
+// 			Usage: "Who to say hello to.",
+// 		},
+// 	}
+// 	//define the action to run
+// 	app.Action = func(c *cli.Context) error {
+// 		name := c.GlobalString("name")
+// 		fmt.Printf("Hello %s!\n", name)
+// 		return nil
+// 	}
+// 	//run the application
+// 	app.Run(os.Args)
+// }
