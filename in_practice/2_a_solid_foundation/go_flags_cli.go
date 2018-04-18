0f3ab33c565b45c7671bf13abd33143d0a977b76
Leland Krych <Leland.Krych@gmail.com>
2018-04-18 15:56:59 -0700

    fix up go_flags_cli

--- in_practice/2_a_solid_foundation/go_flags_cli.go
+++ in_practice/2_a_solid_foundation/go_flags_cli.go
@@ -1,16 +1,22 @@
-package main
+// package main
 
-var opts struct {
-	Name string `short:"n" long:"name default: "World" description:"A name to say hello to."`
-	Spanish bool `short:"s" long:"spanish description:"Use Spanish Language"`
-}
+// import (
+// 	"fmt"
 
-func main {
-	flags.parse(&opts)
+// 	flags "github.com/jessevdk/go-flags"
+// )
 
-	if opts.Spanish == true {
-		fmt.Printf("Hola %s!\n", opts.Name)
-	} else {
-		fmt.Printf("Hello %s!\n", opts.Name)
-	}
-}
+// var opts struct {
+// 	Name    string `short:"n" long:"name" default: "World" description:"A name to say hello to."`
+// 	Spanish bool   `short:"s" long:"spanish" description:"Use Spanish Language"`
+// }
+
+// func main() {
+// 	flags.Parse(&opts)
+
+// 	if opts.Spanish == true {
+// 		fmt.Printf("Hola %s!\n", opts.Name)
+// 	} else {
+// 		fmt.Printf("Hello %s!\n", opts.Name)
+// 	}
+// }
