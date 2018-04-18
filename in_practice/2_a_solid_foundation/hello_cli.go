e27ce15c73145a69f3d1f554f19526ea9bf33254
Leland Krych <Leland.Krych@gmail.com>
2018-04-18 15:57:15 -0700

    use cli framework to build simple hello app

---
+++ in_practice/2_a_solid_foundation/hello_cli.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/urfave/cli"
+
+func main() {
+	//create a new application
+	app := cli.NewApp()
+	app.Name = "hello_cli"
+	app.Usage = "Print hello world"
+	//setup global flag
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "name, n",
+
+			Value: "World",
+			Usage: "Who to say hello to."
+		},
+	}
+	//define the action to run
+	app.Action = func(c *cli.Context) error {
+		name := c.GlobalString("name")
+		fmt.Printf("Hello %s!\n", name)
+		return nil
+	}
+	//run the application
+	app.Run(os.Args)
+}
