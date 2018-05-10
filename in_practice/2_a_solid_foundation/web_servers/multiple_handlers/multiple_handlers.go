8cd457276572624988ea1c6252a52a3cfe9a2ff5
Leland Krych <Leland.Krych@gmail.com>
2018-05-10 14:19:34 -0700

    demonstrate how to use multiple function handlers

---
+++ in_practice/2_a_solid_foundation/web_servers/multiple_handlers/multiple_handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/goodbye/", goodbye) //extra slash is essential here
+	http.HandleFunc("/", homePage)
+	fmt.Println("Serving on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(res, "Hello, my name is ", name)
+}
+
+func goodbye(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+	fmt.Println("Receiving ", parts)
+	name := parts[2]
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(res, "Goodbye ", name)
+}
+
+func homePage(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+	fmt.Fprint(res, "The homepage. You can go to /hello or /goodbye")
+}
