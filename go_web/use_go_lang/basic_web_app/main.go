1dac312aa620772defd2db38cfab5534ebf0afc4
Leland Krych <Leland.Krych@gmail.com>
2018-06-07 12:55:03 -0700

    build faq handler

--- go_web/use_go_lang/basic_web_app/main.go
+++ go_web/use_go_lang/basic_web_app/main.go
@@ -15,12 +15,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send an email "+"to <a href=\"mailto:support@lenslocked.com\">"+"support@lenslocked.com</a>")
+}
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<ul><li>Q: How do I get home?</li> <li>A: Search for the URL http://localhost:3000/ or click <a href=\"http://localhost:3000/\">here</a></li></ul>")
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
