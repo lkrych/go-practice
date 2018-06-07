fb27c44a53ce70a353a27919902d09a65c870a40
Leland Krych <Leland.Krych@gmail.com>
2018-06-07 13:03:48 -0700

    add a custom 404 page to the server

--- go_web/use_go_lang/basic_web_app/main.go
+++ go_web/use_go_lang/basic_web_app/main.go
@@ -22,10 +22,17 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<ul><li>Q: How do I get home?</li> <li>A: Search for the URL http://localhost:3000/ or click <a href=\"http://localhost:3000/\">here</a></li></ul>")
 }
 
+func fourOhfour(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>🌼 Whoopsy Daisy 🌼</h1><p>This isn't what you were looking for! Get on back <a href=\"http://localhost:3000/\">home</a>!</p>")
+}
+
 func main() {
+	var h http.Handler = http.HandlerFunc(fourOhfour)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
 }
