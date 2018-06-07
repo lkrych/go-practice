9c7c7c4fc185ba320324edcef77e4a7fba2438ec
Leland Krych <Leland.Krych@gmail.com>
2018-06-07 11:35:39 -0700

    add more exercise practice

--- exercises/go_exercises/exercises.go
+++ exercises/go_exercises/exercises.go
@@ -1,114 +1,52 @@
 package goExercises
 
-import (
-	"math"
-	"strconv"
-	"strings"
-)
-
-//In O(n) time, and using O(1) space, remove duplicates from an array
-func removeDups(arr []int) []int {
-	nonRepeatIdx := 1
-	for i := 0; i < len(arr)-1; i++ {
-		if arr[i] != arr[i+1] {
-			arr[nonRepeatIdx] = arr[i+1]
-			nonRepeatIdx++
-		}
-	}
-	return arr[:nonRepeatIdx]
-}
-
-//convert base ten int n, into hexadecimal or binary representation
-func baseConverter(n int, base int) string {
-	stringNum := []string{}
-	hexMap := map[int]string{
-		0:  "0",
-		1:  "1",
-		2:  "2",
-		3:  "3",
-		4:  "4",
-		5:  "5",
-		6:  "6",
-		7:  "7",
-		8:  "8",
-		9:  "9",
-		10: "a",
-		11: "b",
-		12: "c",
-		13: "d",
-		14: "e",
-		15: "f",
-	}
-	for n > 0 {
-		if base == 2 {
-			stringNum = append(stringNum, strconv.Itoa(n%2))
-		} else {
-			stringNum = append(stringNum, hexMap[n%16])
-		}
-		n = n / base
-	}
-	return reverse(strings.Join(stringNum, ""))
-}
-
-func reverse(s string) string {
-	sArr := []rune(s)
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		sArr[i], sArr[j] = sArr[j], sArr[i]
-	}
-	return string(sArr)
-}
-
-//TAKE AWAY:  This algorithm should work in a language that has finite-precision arithmetic
-//Arrays can be used to break up numbers of arbitrary size, this allows us to handle very big numbers!
-func multiplyArb(arr1 []int, arr2 []int) []int {
-	sign := 1
-	//check if numbers are negative
-	if (arr1[0] * arr2[0]) > 0 {
-		sign = 1
+//return sum of n + n-1 + ... 1
+func sumNums(n int) int {
+	if n <= 1 {
+		return n
 	} else {
-		sign = -1
+		return n + sumNums(n-1)
 	}
-	//set to absolute val
-	arr1[0] = int(math.Abs(float64(arr1[0])))
-	arr2[0] = int(math.Abs(float64(arr2[0])))
-
-	total := make([]int, len(arr1)+len(arr2))
-	for i := len(arr1) - 1; i >= 0; i-- {
-		for j := len(arr2) - 1; j >= 0; j-- {
-			total[i+j+1] += arr1[i] * arr2[j]
-			total[i+j] += total[i+j+1] / 10
-			total[i+j+1] %= 10
-		}
-	}
-	zeroIdx := 0
-	for {
-		if total[zeroIdx] != 0 {
-			break
-		}
-		zeroIdx++
-	}
-	total = total[zeroIdx:]
-	total[0] = total[0] * sign
-
-	return total
 }
 
-//return true if n is a power of two
-func isPowerOfTwo(n float64) bool {
-	pow := 0
-	for float64(power(2, pow)) <= n {
-		if float64(power(2, pow)) == n {
-			return true
+//return if the word is a palindrome
+func isPalindrome(word string) bool {
+	for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
+		if word[i] != word[j] {
+			return false
 		}
-		pow++
 	}
-	return false
+	return true
 }
 
-func power(base int, exp int) int {
-	if exp == 0 {
-		return 1
-	} else {
-		return base * power(base, exp-1)
+//return the nth prime
+func nthPrime(nth int) int {
+	primeCount := 0
+	checkifPrime := 2
+	for primeCount < nth {
+		if isPrime(checkifPrime) {
+			primeCount++
+		}
+		checkifPrime++
 	}
+	return checkifPrime - 1
+}
+
+func isPrime(n int) bool {
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+//take an array of ints of length word, and return a string with each character of word in the order of idxArray
+func scrambleString(word string, idxArray []int) string {
+	scrambled := make([]rune, len(word))
+	runes := []rune(word)
+	for i, el := range idxArray {
+		scrambled[i] = runes[el]
+	}
+	return string(scrambled)
 }
--- exercises/go_exercises/exercises_test.go
+++ exercises/go_exercises/exercises_test.go
@@ -2,125 +2,81 @@ package goExercises
 
 import (
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
 )
 
-func TestRemoveDups(t *testing.T) {
-	test1 := removeDups([]int{1, 1, 1, 2, 3, 4, 4, 4, 5})
-	test2 := removeDups([]int{1, 1, 1, 2, 33, 102, 102})
-	test3 := removeDups([]int{1, 1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 8, 9, 9})
-	test4 := removeDups([]int{1, 1, 1, 2, 3, 3, 3, 3, 3})
-	test5 := removeDups([]int{1, 1, 1, 2, 3, 3, 4, 5, 5})
-	ans1 := []int{1, 2, 3, 4, 5}
-	ans2 := []int{1, 2, 33, 102}
-	ans3 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	ans4 := []int{1, 2, 3}
-	ans5 := []int{1, 2, 3, 4, 5}
-	if !cmp.Equal(test1, ans1) {
-		t.Errorf("Expected array %v to be %v", test1, ans1)
-	}
-	if !cmp.Equal(test2, ans2) {
-		t.Errorf("Expected array %v to be %v", test2, ans2)
+func TestSumNums(t *testing.T) {
+	test1 := sumNums(1)
+	test2 := sumNums(2)
+	test3 := sumNums(5)
+
+	if test1 != 1 {
+		t.Errorf("Expected sumNums(1) to equal 1, not %v", test1)
 	}
 
-	if !cmp.Equal(test3, ans3) {
-		t.Errorf("Expected array %v to be %v", test4, ans3)
+	if test2 != 3 {
+		t.Errorf("Expected sumNums(2) to equal 3, not %v", test2)
 	}
 
-	if !cmp.Equal(test4, ans4) {
-		t.Errorf("Expected array %v to be %v", test5, ans4)
-	}
-
-	if !cmp.Equal(test5, ans5) {
-		t.Errorf("Expected array %v to be %v", test5, ans5)
+	if test3 != 15 {
+		t.Errorf("Expected sumNums(5) to equal 15, not %v", test3)
 	}
 }
 
-func TestBaseConverter(t *testing.T) {
-	test1 := baseConverter(5, 2)
-	test2 := baseConverter(1239449, 16)
-	test3 := baseConverter(112, 2)
-	test4 := baseConverter(112, 16)
+func TestIsPalindrome(t *testing.T) {
+	test1 := isPalindrome("abc")
+	test2 := isPalindrome("racecar")
+	test3 := isPalindrome("z")
 
-	if test1 != "101" {
-		t.Errorf("Expected baseConverter of 5 to base 2 to equal 101, not %v", test1)
-	}
-	if test2 != "12e999" {
-		t.Errorf("Expected baseConverter of 1239449 to base 16 to be 12e999, not %v", test2)
-	}
-
-	if test3 != "1110000" {
-		t.Errorf("Expected baseConverter of 112 to base 2 to be 1110000, not %v", test3)
-	}
-
-	if test4 != "70" {
-		t.Errorf("Expected baseConverter of 112 to base 16 to be 70, not %v", test4)
-	}
-}
-
-func TestMultiplyArbitrary(t *testing.T) {
-	test1 := multiplyArb([]int{1, 9, 3, 7, 0, 7, 7, 2, 1}, []int{-7, 6, 1, 8, 3, 8, 2, 5, 7, 2, 8, 7})
-	test2 := multiplyArb([]int{1, 2, 9, 8, 2, 3, 4, 1, 3, 5, 0}, []int{4, 5})
-	test3 := multiplyArb([]int{1, 2, 5, 3, 2, 4, 6, 7, 8, 2}, []int{-4, 0, 0, 2, 3})
-	test4 := multiplyArb([]int{5, 3, 6, 3, 7, 9, 5, 0}, []int{3, 5, 2})
-	test5 := multiplyArb([]int{1, 2}, []int{5})
-	test6 := multiplyArb([]int{1, 2, 3}, []int{9, 8, 7})
-	ans1 := []int{-1, 4, 7, 5, 7, 3, 9, 5, 2, 5, 8, 9, 6, 7, 6, 4, 1, 2, 9, 2, 7}
-	ans2 := []int{5, 8, 4, 2, 0, 5, 3, 6, 0, 7, 5, 0}
-	ans3 := []int{-5, 0, 1, 5, 8, 6, 9, 5, 9, 5, 5, 9, 8, 6}
-	ans4 := []int{1, 8, 8, 8, 0, 5, 5, 8, 4, 0, 0}
-	ans5 := []int{6, 0}
-	ans6 := []int{1, 2, 1, 4, 0, 1}
-
-	if !cmp.Equal(test1, ans1) {
-		t.Errorf("Expected multiplyArb to multiply the array %v, instead you got %v", ans1, test1)
-	}
-	if !cmp.Equal(test2, ans2) {
-		t.Errorf("Expected multiplyArb to multiply the array %v, instead you got %v", ans2, test2)
-	}
-
-	if !cmp.Equal(test3, ans3) {
-		t.Errorf("Expected multiplyArb to multiply the array %v, instead you got %v", ans3, test3)
-	}
-
-	if !cmp.Equal(test4, ans4) {
-		t.Errorf("Expected multiplyArb to multiply the array %v, instead you got %v", ans4, test4)
-	}
-
-	if !cmp.Equal(test5, ans5) {
-		t.Errorf("Expected multiplyArb to multiply the array %v, instead you got %v", ans5, test5)
-	}
-
-	if !cmp.Equal(test6, ans6) {
-		t.Errorf("Expected multiplyArb to multiply the array %v, instead you got %v", ans6, test6)
-	}
-}
-
-func TestIsPowerOfTwo(t *testing.T) {
-	test1 := isPowerOfTwo(1)
-	test2 := isPowerOfTwo(16)
-	test3 := isPowerOfTwo(64)
-	test4 := isPowerOfTwo(78)
-	test5 := isPowerOfTwo(0)
-
-	if test1 != true {
-		t.Errorf("Expected isPowerOfTwo of 1 to be true, not %v", test1)
+	if test1 != false {
+		t.Errorf("Expected isPalindrome of abc to be false, not %v", test1)
 	}
 
 	if test2 != true {
-		t.Errorf("Expected isPowerOfTwo of 16 to be true, not %v", test2)
+		t.Errorf("Expected isPalindrome of racecar to be true, not %v", test2)
 	}
 
 	if test3 != true {
-		t.Errorf("Expected isPowerOfTwo of 64 to be true, not %v", test3)
+		t.Errorf("Expected isPalindrome of z to be true, not %v", test3)
+	}
+}
+
+func TestNthPrime(t *testing.T) {
+	test1 := nthPrime(3)
+	test2 := nthPrime(4)
+	test3 := nthPrime(15)
+	test4 := nthPrime(28)
+	test5 := nthPrime(77)
+
+	if test1 != 5 {
+		t.Errorf("Expected nthPrime of 3 to be 5, not %v", test1)
 	}
 
-	if test4 != false {
-		t.Errorf("Expected isPowerOfTwo of 78 to be false, not %v", test4)
+	if test2 != 7 {
+		t.Errorf("Expected nthPrime of 4 to be 7, not %v", test2)
 	}
 
-	if test5 != false {
-		t.Errorf("Expected isPowerOfTwo of 0 to be false, not %v", test5)
+	if test3 != 47 {
+		t.Errorf("Expected nthPrime of 15 to be 47, not %v", test3)
+	}
+
+	if test4 != 107 {
+		t.Errorf("Expected nthPrime of 28 to be 107, not %v", test4)
+	}
+
+	if test5 != 389 {
+		t.Errorf("Expected nthPrime of 77 to be 389, not %v", test5)
+	}
+}
+
+func TestScrambleString(t *testing.T) {
+	test1 := scrambleString("abcd", []int{3, 1, 2, 0})
+	test2 := scrambleString("markov", []int{5, 3, 1, 4, 2, 0})
+
+	if test1 != "dbca" {
+		t.Errorf("Expected scrambleString of 'abcd' with inputs [3,1,2,0] to be 'dcba', not %v", test1)
+	}
+
+	if test2 != "vkaorm" {
+		t.Errorf("Expected scrambleString of 'markov' with inputs [5,3,1,4,2,0] to be 'vkaorm', not %v", test2)
 	}
 }
