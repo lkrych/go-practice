c39586ea218bc392a946ff095714b464b5517509
Leland Krych <Leland.Krych@gmail.com>
2018-06-07 11:36:27 -0700

    add negroni middleware to server

--- snippet/api/server.go
+++ snippet/api/server.go
@@ -5,19 +5,23 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func main() {
-	initKeys()
+	InitKeys()
 	r := mux.NewRouter()
 
 	//Public endpoint
-	http.HandleFunc("/login", LoginHandler)
+	r.HandleFunc("/login", LoginHandler)
 
 	// Handle API routes
 	api := r.PathPrefix("/api/").Subrouter()
 
-	api.Handle("/search/{keyword}", searchTermHandler).Methods("GET")
+	api.Handle("/search/{keyword}", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(searchTermHandler)),
+	))
 
 	// Serve static assets directly.
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
