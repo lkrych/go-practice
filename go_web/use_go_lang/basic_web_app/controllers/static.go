061dabe8eef90cffa94b5eafb57f885560d17732
Leland Krych <Leland.Krych@gmail.com>
2018-06-08 12:56:35 -0700

    add 404 to static controller

--- go_web/use_go_lang/basic_web_app/controllers/static.go
+++ go_web/use_go_lang/basic_web_app/controllers/static.go
@@ -12,11 +12,14 @@ func NewStatic() *Static {
 			"bootstrap", "views/static/contact.gohtml"),
 		Faq: views.NewView(
 			"bootstrap", "views/static/faq.gohtml"),
+		FourOhfour: views.NewView(
+			"bootstrap", "views/static/404.gohtml"),
 	}
 }
 
 type Static struct {
-	Home    *views.View
-	Contact *views.View
-	Faq     *views.View
+	Home       *views.View
+	Contact    *views.View
+	Faq        *views.View
+	FourOhfour *views.View
 }
