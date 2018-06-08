6ce9390d2db8f55f1887156042bcb51b7678d9a3
Leland Krych <Leland.Krych@gmail.com>
2018-06-07 21:21:41 -0700

    create #create endpoint in users controller

--- go_web/use_go_lang/basic_web_app/controllers/users.go
+++ go_web/use_go_lang/basic_web_app/controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"flatphoto.com/views"
@@ -12,6 +13,15 @@ func NewUsers() *Users {
 	}
 }
 
+type Users struct {
+	NewView *views.View
+}
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 //New is used to render the form where a user can create a new user account
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +30,15 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type Users struct {
-	NewView *views.View
+//Create is used to process the signup form when a user tries to create a new user account
+//POST /signup
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Email is", form.Email)
+	fmt.Fprintln(w, "Password is", form.Password)
+
 }
