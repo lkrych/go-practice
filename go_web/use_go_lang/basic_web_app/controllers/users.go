9f4177be499a24cb47da830578e6b1861576b950
Leland Krych <Leland.Krych@gmail.com>
2018-06-08 16:34:12 -0700

    create the signIn method for the user model

--- go_web/use_go_lang/basic_web_app/controllers/users.go
+++ go_web/use_go_lang/basic_web_app/controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"flatphoto.com/models"
+	"flatphoto.com/rand"
 	"flatphoto.com/views"
 )
 
@@ -59,8 +60,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, "User is", user)
-
+	err := u.signIn(w, &user)
+	if err != nil {
+		//temporarily render the error message for debugging
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//redirect to the cookie test page to test the cookie
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 //Login is used to process the login form when a user tries to login as an existing user
@@ -82,20 +89,48 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	cookie := http.Cookie{
-		Name:  "email",
-		Value: user.Email,
-	}
-	http.SetCookie(w, &cookie)
-	fmt.Fprintln(w, user)
-}
-
-//CookieTest is used to display cookies set on the current user
-func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("email")
+	err = u.signIn(w, user)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintln(w, "Email is:", cookie.Value)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+// signIn is used to sign the given user in via cookies
+func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+		err = u.us.Update(user)
+		if err != nil {
+			return err
+		}
+	}
+	cookie := http.Cookie{
+		Name:     "remember_token",
+		Value:    user.Remember,
+		HttpOnly: true, //don't allow js to interact with cookies
+	}
+	http.SetCookie(w, &cookie)
+	return nil
+}
+
+//CookieTest is used to display cookies set on the current user
+func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("remember_token")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user, err := u.us.ByRemember(cookie.Value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "The user is", user.Username)
+	fmt.Fprintln(w, "remember_token is:", cookie.Value)
 }
