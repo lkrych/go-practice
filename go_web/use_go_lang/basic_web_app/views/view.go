e022dbdd1012af2b64107e7a7e4c619b666e2bb0
Leland Krych <Leland.Krych@gmail.com>
2018-06-07 21:22:14 -0700

    add serveHTTP method so that View can fulfill router interface

--- go_web/use_go_lang/basic_web_app/views/view.go
+++ go_web/use_go_lang/basic_web_app/views/view.go
@@ -40,5 +40,13 @@ type View struct {
 }
 
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "text/html")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+//implement router interface for static actions
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := v.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
