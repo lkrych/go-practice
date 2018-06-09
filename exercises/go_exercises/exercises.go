f7d1cb63c8fb2846f7d6b0698e42b99c9059294f
Leland Krych <Leland.Krych@gmail.com>
2018-06-09 10:01:35 -0700

    Add exercise practice

--- exercises/go_exercises/exercises.go
+++ exercises/go_exercises/exercises.go
@@ -7,89 +7,94 @@ import (
 	"strings"
 )
 
+//return a string where every char in sentence is shifted by shift
+func caesarCipher(shift int, sentence string) string {
+	alpha := "abcdefghijklmnopqrstuvwxyz"
+	cipher := make([]byte, len(sentence))
+	for i, el := range strings.Split(sentence, "") {
+		currIdx := strings.Index(alpha, el)
+		if currIdx == -1 {
+			cipher[i] = ' '
+			continue
+		}
+		cipher[i] = alpha[(currIdx+shift)%26]
+	}
+	return string(cipher)
+}
+
+//Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+func isUnique(word string) bool {
+	alphaMap := make(map[string]bool)
+	for _, el := range strings.Split(word, "") {
+		if _, ok := alphaMap[el]; ok {
+			return false
+		} else {
+			alphaMap[el] = true
+		}
+	}
+	return true
+}
+
+//return the nth prime
+func nthPrime(nth int) int {
+	n := 0
+	checkIfPrime := 2
+	for n < nth {
+		if isPrime(checkIfPrime) {
+			n++
+		}
+		checkIfPrime++
+	}
+	return checkIfPrime - 1
+}
+
+func isPrime(n int) bool {
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 //add dashes around an odd number, except on first or last el. Only allow one dash
 //ex: 303 => 3-0-3
 //ex: 333 => 3-3-3
 //ex: 3223 => 3-22-3
 func dasherizeNumber(n int) string {
-	stringArr := []string{}
+	intString := []string{}
 	for n > 0 {
-		el := n % 10
-		if el%2 == 0 {
-			stringArr = append(stringArr, strconv.Itoa(el))
+		digit := n % 10
+		if digit%2 == 0 {
+			intString = append(intString, strconv.Itoa(digit))
 		} else {
-			stringArr = append(stringArr, "-")
-			stringArr = append(stringArr, strconv.Itoa(el))
-			stringArr = append(stringArr, "-")
+			intString = append(intString, "-")
+			intString = append(intString, strconv.Itoa(digit))
+			intString = append(intString, "-")
 		}
 		n = n / 10
 	}
 
-	//strip front and last
-	if stringArr[0] == "-" {
-		stringArr = stringArr[1:]
+	//remove first dash and last dash
+	if intString[0] == "-" {
+		intString = intString[1:]
 	}
 
-	if stringArr[len(stringArr)-1] == "-" {
-		stringArr = stringArr[:len(stringArr)-1]
+	if intString[len(intString)-1] == "-" {
+		intString = intString[:len(intString)-1]
 	}
 
-	stringed := strings.Join(stringArr, "")
-
-	//remove double
+	//remove double dashes
 	r, err := regexp.Compile("--")
 	if err != nil {
-		log.Fatal("Couldn't compile regexp")
+		log.Fatal("Regexp didn't compile")
 	}
+	stringed := strings.Join(intString, "")
 
 	stringed = r.ReplaceAllString(stringed, "-")
 	return reverse(stringed)
 }
 
-// calculate the checksum of the multi-dimensional array
-// For each row, determine the difference between the largest value and the smallest value;
-// the checksum is the sum of all of these differences.
-func checkSum(multiArr [][]int) int {
-	checkSum := 0
-	for _, row := range multiArr {
-		colMax := 0
-		colMin := 100000
-		for _, col := range row {
-			if col > colMax {
-				colMax = col
-			}
-
-			if col < colMin {
-				colMin = col
-			}
-		}
-		checkSum += (colMax - colMin)
-	}
-	return checkSum
-}
-
-func checkSumAdvent() int {
-	//read in the file ../practice/checksum_input and find the checksum for it
-	return 0
-}
-
-//return num of vowels in sentence
-func countVowels(sentence string) int {
-	r, err := regexp.Compile("[aeiou]")
-	if err != nil {
-		log.Fatal("Failed to compile regexp")
-	}
-	chars := strings.Split(sentence, "")
-	vowelCount := 0
-	for _, char := range chars {
-		if r.MatchString(char) {
-			vowelCount++
-		}
-	}
-	return vowelCount
-}
-
-//take sentence and reverse it, do not use library method
 func reverse(sentence string) string {
 	r := []rune(sentence)
 	for i, j := 0, len(sentence)-1; i < j; i, j = i+1, j-1 {
--- exercises/go_exercises/exercises_test.go
+++ exercises/go_exercises/exercises_test.go
@@ -4,6 +4,80 @@ import (
 	"testing"
 )
 
+func TestCaesarCipher(t *testing.T) {
+	test1 := caesarCipher(3, "abc")
+	test2 := caesarCipher(3, "abc xyz")
+	test3 := caesarCipher(29, "abc xyz")
+
+	if test1 != "def" {
+		t.Errorf("Expected caesarCipher with a shift of 3 and input of abc to be def, not %v", test1)
+	}
+
+	if test2 != "def abc" {
+		t.Errorf("Expected caesarCipher with a shift of 3 and input of abc xyz to be def abc, not %v", test2)
+	}
+
+	if test3 != "def abc" {
+		t.Errorf("Expected caesarCipher with a shift of 29 and input of abc xyz to be def abc, not %v", test3)
+	}
+}
+
+func TestisUnique(t *testing.T) {
+	test1 := isUnique("blah")
+	test2 := isUnique("banana")
+	test3 := isUnique("fike hyjrant")
+	test4 := isUnique("fire hydrant")
+	test5 := isUnique("Another Day")
+
+	if test1 != true {
+		t.Errorf("Expected blah to be true because all letters are unique ")
+	}
+
+	if test2 != false {
+		t.Errorf("Expected banana to be false because all letters are not unique ")
+	}
+
+	if test3 != true {
+		t.Errorf("Expected fike hyjrant to be true because all letters are unique ")
+	}
+
+	if test4 != false {
+		t.Errorf("Expected fire hydrant to be false because all letters are not unique ")
+	}
+
+	if test5 != false {
+		t.Errorf("Expected Another day to be false because all letters are not unique ")
+	}
+}
+
+func TestNthPrime(t *testing.T) {
+	test1 := nthPrime(3)
+	test2 := nthPrime(4)
+	test3 := nthPrime(15)
+	test4 := nthPrime(28)
+	test5 := nthPrime(77)
+
+	if test1 != 5 {
+		t.Errorf("Expected nthPrime of 3 to be 5, not %v", test1)
+	}
+
+	if test2 != 7 {
+		t.Errorf("Expected nthPrime of 4 to be 7, not %v", test2)
+	}
+
+	if test3 != 47 {
+		t.Errorf("Expected nthPrime of 15 to be 47, not %v", test3)
+	}
+
+	if test4 != 107 {
+		t.Errorf("Expected nthPrime of 28 to be 107, not %v", test4)
+	}
+
+	if test5 != 389 {
+		t.Errorf("Expected nthPrime of 77 to be 389, not %v", test5)
+	}
+}
+
 func TestDasherizeNumber(t *testing.T) {
 	test1 := dasherizeNumber(203)
 	test2 := dasherizeNumber(303)
@@ -25,68 +99,4 @@ func TestDasherizeNumber(t *testing.T) {
 	if test4 != "3-22-3" {
 		t.Errorf("Expected dasherizeNumber of 3223  to be 3-22-3, not %v", test4)
 	}
-}
-
-func TestCheckSum(t *testing.T) {
-	test1 := checkSum([][]int{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}})
-	test2 := checkSum([][]int{{1, 2, 3, 4}, {1, 2, 3}, {1, 2, 3}})
-	test3 := checkSum([][]int{{1, 2, 3, 10}, {1, 2, 3, 12}, {1, 2, 3, 15}})
-	test4 := checkSum([][]int{{3, 22, 124}, {3, 70}, {1, 2, 5}})
-	test5 := checkSum([][]int{{1, 2, 10}, {1, 11, 3}, {500, 2, 3}})
-	// test6 := checkSum(checkSumAdvent())
-
-	if test1 != 6 {
-		t.Errorf("Expected answer to be 6, not %v", test1)
-	}
-	if test2 != 7 {
-		t.Errorf("Expected answer to be 7, not %v", test2)
-	}
-
-	if test3 != 34 {
-		t.Errorf("Expected answer to be 34, not %v", test3)
-	}
-
-	if test4 != 192 {
-		t.Errorf("Expected answer to be 192, not %v", test4)
-	}
-
-	if test5 != 517 {
-		t.Errorf("Expected answer to be 517, not %v", test5)
-	}
-
-	// if test6 != 53978 {
-	// 	t.Errorf("Expected answer to be 53978, not %v", test6)
-	// }
-}
-
-func TestCountVowels(t *testing.T) {
-	test1 := countVowels("abcd")
-	test2 := countVowels("colour")
-	test3 := countVowels("cecilia nutley")
-
-	if test1 != 1 {
-		t.Errorf("Expected countVowels of abcd to be 1, not %v", test1)
-	}
-
-	if test2 != 3 {
-		t.Errorf("Expected countVowels of colour to be 3, not %v", test2)
-	}
-
-	if test3 != 6 {
-		t.Errorf("Expected countVowels of mom to be 6, not %v", test3)
-	}
-}
-
-func TestReverse(t *testing.T) {
-	word := reverse("candelabra")
-	sentence := reverse("reverse this sentence")
-
-	if word != "arbalednac" {
-		t.Errorf("Expected reverse to be able to reverse a string")
-	}
-
-	if sentence != "ecnetnes siht esrever" {
-		t.Errorf("Expected reverse to be able to reverse a sentence, but got %v", sentence)
-	}
-
 }
