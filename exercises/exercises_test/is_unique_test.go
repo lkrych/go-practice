25348ba35cf4a04fcb05c1efc2de6e8002478236
Leland Krych <Leland.Krych@gmail.com>
2018-06-09 09:56:33 -0700

    fix errors in isUnique problem

--- exercises/exercises_test/is_unique_test.go
+++ exercises/exercises_test/is_unique_test.go
@@ -8,22 +8,22 @@ func TestisUnique(t *testing.T) {
 	test5 := isUnique("Another Day")
 
 	if test1 != true {
-		t.ErrorF("Expected blah to be true because all letters are unique ")
+		t.Errorf("Expected blah to be true because all letters are unique ")
 	}
 
 	if test2 != false {
-		t.ErrorF("Expected banana to be false because all letters are not unique ")
+		t.Errorf("Expected banana to be false because all letters are not unique ")
 	}
 
 	if test3 != true {
-		t.ErrorF("Expected fike hyjrant to be true because all letters are unique ")
+		t.Errorf("Expected fike hyjrant to be true because all letters are unique ")
 	}
 
 	if test4 != false {
-		t.ErrorF("Expected fire hydrant to be false because all letters are not unique ")
+		t.Errorf("Expected fire hydrant to be false because all letters are not unique ")
 	}
 
 	if test5 != false {
-		t.ErrorF("Expected Another day to be false because all letters are not unique ")
+		t.Errorf("Expected Another day to be false because all letters are not unique ")
 	}
 }
