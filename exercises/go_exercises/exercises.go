6fe87cd55501c6f607d0cc9ebfa1a86e9f0dc90f
Leland Krych <Leland.Krych@gmail.com>
2018-06-10 10:07:06 -0700

    add more exercises to the roster

--- exercises/go_exercises/exercises.go
+++ exercises/go_exercises/exercises.go
@@ -1,104 +1,118 @@
 package goExercises
 
 import (
-	"log"
-	"regexp"
-	"strconv"
-	"strings"
+	"fmt"
+	"math"
 )
 
-//return a string where every char in sentence is shifted by shift
-func caesarCipher(shift int, sentence string) string {
-	alpha := "abcdefghijklmnopqrstuvwxyz"
-	cipher := make([]byte, len(sentence))
-	for i, el := range strings.Split(sentence, "") {
-		currIdx := strings.Index(alpha, el)
-		if currIdx == -1 {
-			cipher[i] = ' '
-			continue
-		}
-		cipher[i] = alpha[(currIdx+shift)%26]
-	}
-	return string(cipher)
-}
-
-//Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
-func isUnique(word string) bool {
-	alphaMap := make(map[string]bool)
-	for _, el := range strings.Split(word, "") {
-		if _, ok := alphaMap[el]; ok {
-			return false
+//take in an array of digits encoding a decimal, d and update the array to rep d + 1
+//ex: 1.29 => 1.30 ~ [1,2,9] => [1,3,0]
+//ex 1.48372 => 1.48373
+//TAKE AWAY:  This algorithm should work in a language that has finite-precision arithmetic
+//Arrays can be used to break up numbers of arbitrary size, this allows us to handle very big numbers!
+func incrementArb(arr []int) []int {
+	lastIdx := len(arr) - 1
+	carry := (1 + arr[lastIdx]) / 10
+	arr[lastIdx] = (1 + arr[lastIdx]) % 10
+forLoop:
+	for i := lastIdx - 1; i > 0; i-- {
+		if carry > 0 {
+			sum := carry + arr[i]
+			carry = sum / 10
+			arr[i] = sum % 10
 		} else {
-			alphaMap[el] = true
+			break forLoop
 		}
 	}
-	return true
+	return arr
 }
 
-//return the nth prime
-func nthPrime(nth int) int {
-	n := 0
-	checkIfPrime := 2
-	for n < nth {
-		if isPrime(checkIfPrime) {
-			n++
-		}
-		checkIfPrime++
-	}
-	return checkIfPrime - 1
-}
-
-func isPrime(n int) bool {
-	for i := 2; i < n; i++ {
-		if n%i == 0 {
+//return if the word is a palindrome
+func isPalindrome(word string) bool {
+	for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
+		if word[i] != word[j] {
 			return false
 		}
 	}
 	return true
 }
 
-//add dashes around an odd number, except on first or last el. Only allow one dash
-//ex: 303 => 3-0-3
-//ex: 333 => 3-3-3
-//ex: 3223 => 3-22-3
-func dasherizeNumber(n int) string {
-	intString := []string{}
-	for n > 0 {
-		digit := n % 10
-		if digit%2 == 0 {
-			intString = append(intString, strconv.Itoa(digit))
-		} else {
-			intString = append(intString, "-")
-			intString = append(intString, strconv.Itoa(digit))
-			intString = append(intString, "-")
-		}
-		n = n / 10
+//return sorted arr using mergesort
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 { //arr is sorted!
+		return arr
 	}
-
-	//remove first dash and last dash
-	if intString[0] == "-" {
-		intString = intString[1:]
-	}
-
-	if intString[len(intString)-1] == "-" {
-		intString = intString[:len(intString)-1]
-	}
-
-	//remove double dashes
-	r, err := regexp.Compile("--")
-	if err != nil {
-		log.Fatal("Regexp didn't compile")
-	}
-	stringed := strings.Join(intString, "")
-
-	stringed = r.ReplaceAllString(stringed, "-")
-	return reverse(stringed)
+	midIdx := len(arr) / 2
+	leftMerge := mergeSort(arr[:midIdx])
+	rightMerge := mergeSort(arr[midIdx:])
+	return merge(leftMerge, rightMerge)
 }
 
-func reverse(sentence string) string {
-	r := []rune(sentence)
-	for i, j := 0, len(sentence)-1; i < j; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
+func merge(left, right []int) []int {
+	merged := []int{}
+	for len(left) > 0 && len(right) > 0 {
+
+		if left[0] <= right[0] {
+			merged = append(merged, left[0])
+			left = left[1:]
+		} else {
+			merged = append(merged, right[0])
+			right = right[1:]
+		}
 	}
-	return string(r)
+	if len(left) == 0 {
+		merged = append(merged, right...)
+		right = []int{}
+	} else if len(right) == 0 {
+		merged = append(merged, left...)
+		left = []int{}
+	}
+	return merged
+}
+
+//return true if n is a power of two
+func isPowerOfTwo(n float64) bool {
+	exp := 0
+	for pow(2, exp) <= n {
+		if pow(2, exp) == n {
+			return true
+		}
+		exp++
+	}
+	return false
+}
+
+func pow(base, exp int) float64 {
+	if exp == 0 {
+		return 1
+	} else if exp == 1 {
+		return float64(base)
+	} else {
+		return float64(base) * pow(base, exp-1)
+	}
+}
+
+//think of this exercise like a variation of mancala.
+//Starting at the zeroth index, can you navigate to the last index using the number of hops given to you at each index?
+//You may use less than the number of hops given to you at each index
+//return true if you can, return false if it is impossible
+//ex [3,3,1,0,2,0,1] is true
+// hop once from 3 to 3, hop three times to two and hop twice to 1!
+//ex [3,2,0,0,2,0,1] is false
+// the farthest you can hop is to index 4
+func advancingArray(arr []int) bool {
+	farthestTraveled := arr[0]
+	for i := 0; i <= farthestTraveled; i++ {
+		fmt.Println("the board is", arr)
+		fmt.Println("Checking idx", i)
+		if farthestTraveled == len(arr)-1 {
+			fmt.Println("maneuvered to the end")
+			return true
+		}
+		fmt.Println("Comparing farthestTraveled", farthestTraveled, "to ", arr[i]+i)
+
+		farthestTraveled = int(math.Max(float64(farthestTraveled), float64(arr[i]+i)))
+	}
+	fmt.Println("didn't find the end")
+	return false
 }
--- exercises/go_exercises/exercises_test.go
+++ exercises/go_exercises/exercises_test.go
@@ -2,101 +2,142 @@ package goExercises
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestCaesarCipher(t *testing.T) {
-	test1 := caesarCipher(3, "abc")
-	test2 := caesarCipher(3, "abc xyz")
-	test3 := caesarCipher(29, "abc xyz")
+func TestIncrementArbitrary(t *testing.T) {
+	test1 := incrementArb([]int{1, 2, 9})
+	test2 := incrementArb([]int{1, 2, 9, 8, 2, 3, 4, 1, 3, 5, 0})
+	test3 := incrementArb([]int{1, 2, 5, 3, 2, 4, 6, 7, 8, 2})
+	test4 := incrementArb([]int{5, 3, 6, 3, 7, 9, 5, 0})
+	test5 := incrementArb([]int{1, 2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9})
+	ans1 := []int{1, 3, 0}
+	ans2 := []int{1, 2, 9, 8, 2, 3, 4, 1, 3, 5, 1}
+	ans3 := []int{1, 2, 5, 3, 2, 4, 6, 7, 8, 3}
+	ans4 := []int{5, 3, 6, 3, 7, 9, 5, 1}
+	ans5 := []int{1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
-	if test1 != "def" {
-		t.Errorf("Expected caesarCipher with a shift of 3 and input of abc to be def, not %v", test1)
+	if !cmp.Equal(test1, ans1) {
+		t.Errorf("Expected incrementArb to increment the array %v, instead you got %v", ans1, test1)
+	}
+	if !cmp.Equal(test2, ans2) {
+		t.Errorf("Expected incrementArb to increment the array %v, instead you got %v", ans2, test2)
 	}
 
-	if test2 != "def abc" {
-		t.Errorf("Expected caesarCipher with a shift of 3 and input of abc xyz to be def abc, not %v", test2)
+	if !cmp.Equal(test3, ans3) {
+		t.Errorf("Expected incrementArb to increment the array %v, instead you got %v", ans3, test3)
 	}
 
-	if test3 != "def abc" {
-		t.Errorf("Expected caesarCipher with a shift of 29 and input of abc xyz to be def abc, not %v", test3)
+	if !cmp.Equal(test4, ans4) {
+		t.Errorf("Expected incrementArb to increment the array %v, instead you got %v", ans4, test4)
+	}
+
+	if !cmp.Equal(test5, ans5) {
+		t.Errorf("Expected incrementArb to increment the array %v, instead you got %v", ans5, test5)
 	}
 }
 
-func TestisUnique(t *testing.T) {
-	test1 := isUnique("blah")
-	test2 := isUnique("banana")
-	test3 := isUnique("fike hyjrant")
-	test4 := isUnique("fire hydrant")
-	test5 := isUnique("Another Day")
+func TestIsPalindrome(t *testing.T) {
+	test1 := isPalindrome("abc")
+	test2 := isPalindrome("racecar")
+	test3 := isPalindrome("z")
 
-	if test1 != true {
-		t.Errorf("Expected blah to be true because all letters are unique ")
+	if test1 != false {
+		t.Errorf("Expected isPalindrome of abc to be false, not %v", test1)
 	}
 
-	if test2 != false {
-		t.Errorf("Expected banana to be false because all letters are not unique ")
+	if test2 != true {
+		t.Errorf("Expected isPalindrome of racecar to be true, not %v", test2)
 	}
 
 	if test3 != true {
-		t.Errorf("Expected fike hyjrant to be true because all letters are unique ")
+		t.Errorf("Expected isPalindrome of z to be true, not %v", test3)
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	test1 := mergeSort([]int{7, 5, 2, 4, 8, 9, 3})
+	test2 := mergeSort([]int{7, 5, 2, 4, 8, 9, 3, 1, 6, 10})
+	test3 := mergeSort([]int{7, 5, 2, 4, 8, 9, 3, 3, 3, 3})
+	test4 := mergeSort([]int{7, 5, 2, 4, 8, 9, 3, 100, 102, 6, 1, 10})
+	test5 := mergeSort([]int{7, 5, 2, 4, 8, 9, 3, 11, 10, 6, 1})
+	ans1 := []int{2, 3, 4, 5, 7, 8, 9}
+	ans2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	ans3 := []int{2, 3, 3, 3, 3, 4, 5, 7, 8, 9}
+	ans4 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 102}
+	ans5 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+
+	if !cmp.Equal(test1, ans1) {
+		t.Errorf("Expected mergeSort to sort the array %v, instead you got %v", ans1, test1)
+	}
+	if !cmp.Equal(test2, ans2) {
+		t.Errorf("Expected mergeSort to sort the array %v, instead you got %v", ans2, test2)
+	}
+
+	if !cmp.Equal(test3, ans3) {
+		t.Errorf("Expected mergeSort to sort the array %v, instead you got %v", ans3, test3)
+	}
+
+	if !cmp.Equal(test4, ans4) {
+		t.Errorf("Expected mergeSort to sort the array %v, instead you got %v", ans4, test4)
+	}
+
+	if !cmp.Equal(test5, ans5) {
+		t.Errorf("Expected mergeSort to sort the array %v, instead you got %v", ans5, test5)
+	}
+}
+
+func TestIsPowerOfTwo(t *testing.T) {
+	test1 := isPowerOfTwo(1)
+	test2 := isPowerOfTwo(16)
+	test3 := isPowerOfTwo(64)
+	test4 := isPowerOfTwo(78)
+	test5 := isPowerOfTwo(0)
+
+	if test1 != true {
+		t.Errorf("Expected isPowerOfTwo of 1 to be true, not %v", test1)
+	}
+
+	if test2 != true {
+		t.Errorf("Expected isPowerOfTwo of 16 to be true, not %v", test2)
+	}
+
+	if test3 != true {
+		t.Errorf("Expected isPowerOfTwo of 64 to be true, not %v", test3)
 	}
 
 	if test4 != false {
-		t.Errorf("Expected fire hydrant to be false because all letters are not unique ")
+		t.Errorf("Expected isPowerOfTwo of 78 to be false, not %v", test4)
 	}
 
 	if test5 != false {
-		t.Errorf("Expected Another day to be false because all letters are not unique ")
+		t.Errorf("Expected isPowerOfTwo of 0 to be false, not %v", test5)
 	}
 }
 
-func TestNthPrime(t *testing.T) {
-	test1 := nthPrime(3)
-	test2 := nthPrime(4)
-	test3 := nthPrime(15)
-	test4 := nthPrime(28)
-	test5 := nthPrime(77)
-
-	if test1 != 5 {
-		t.Errorf("Expected nthPrime of 3 to be 5, not %v", test1)
+func TestAdvancingArray(t *testing.T) {
+	test1 := []int{3, 3, 1, 0, 2, 0, 1}
+	test2 := []int{3, 2, 0, 0, 2, 0, 1}
+	test3 := []int{5, 3, 1, 0, 2, 0, 1}
+	test4 := []int{0, 3, 1, 0, 2, 0, 1}
+	test5 := []int{3, 3, 1, 0, 2, 0, 1, 3, 1, 0, 6}
+	if advancingArray(test1) != true {
+		t.Errorf("Expected array %v to be true", test1)
+	}
+	if advancingArray(test2) != false {
+		t.Errorf("Expected array %v to be false", test2)
 	}
 
-	if test2 != 7 {
-		t.Errorf("Expected nthPrime of 4 to be 7, not %v", test2)
+	if advancingArray(test3) != true {
+		t.Errorf("Expected array %v to be true", test3)
 	}
 
-	if test3 != 47 {
-		t.Errorf("Expected nthPrime of 15 to be 47, not %v", test3)
+	if advancingArray(test4) != false {
+		t.Errorf("Expected array %v to be false", test4)
 	}
 
-	if test4 != 107 {
-		t.Errorf("Expected nthPrime of 28 to be 107, not %v", test4)
-	}
-
-	if test5 != 389 {
-		t.Errorf("Expected nthPrime of 77 to be 389, not %v", test5)
-	}
-}
-
-func TestDasherizeNumber(t *testing.T) {
-	test1 := dasherizeNumber(203)
-	test2 := dasherizeNumber(303)
-	test3 := dasherizeNumber(333)
-	test4 := dasherizeNumber(3223)
-
-	if test1 != "20-3" {
-		t.Errorf("Expected dasherizeNumber of 203  to be 20-3, not %v", test1)
-	}
-
-	if test2 != "3-0-3" {
-		t.Errorf("Expected dasherizeNumber of 303  to be 3-0-3, not %v", test2)
-	}
-
-	if test3 != "3-3-3" {
-		t.Errorf("Expected dasherizeNumber of 333 to be 3-3-3, not %v", test3)
-	}
-
-	if test4 != "3-22-3" {
-		t.Errorf("Expected dasherizeNumber of 3223  to be 3-22-3, not %v", test4)
+	if advancingArray(test5) != true {
+		t.Errorf("Expected array %v to be true", test5)
 	}
 }
