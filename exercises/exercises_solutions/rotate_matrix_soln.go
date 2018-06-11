90a2d8bd554b05a347bf6fa2a91d413c1bfa6306
Leland Krych <Leland.Krych@gmail.com>
2018-06-10 17:33:07 -0700

    add rotate_matrix to problem set

---
+++ exercises/exercises_solutions/rotate_matrix_soln.go
@@ -0,0 +1,25 @@
+import "log"
+
+func rotateMatrix(matrix [][]int) [][]int {
+	if matrix.length == 0 || matrix.length != matrix[0].length {
+		log.Fatal("this matrix is not square")
+	}
+	n = len(matrix)
+	for layer := 0; layer < n/2; layer++ {
+		first := layer
+		last = n - 1 - layer
+		for i = first; i < last; i++ {
+			offset := i - first
+			top := matrix[first][i] //save top
+			//rotate left -> top
+			matrix[first][i] = matrix[last-offest][first]
+			//bottom -> left
+			matrix[last-offset][first] = matrix[last][last-offset]
+			//right -> bottom
+			matrix[last][last-offset] = matrix[i][last]
+			//top -> right
+			matrix[i][last] = top
+		}
+	}
+	return matrix
+}
---
+++ exercises/exercises_test/rotate_matrix_test.go
@@ -0,0 +1,23 @@
+import "testing"
+
+func TestRotateMatrix(t *testing.T) {
+	test1 := rotateMatrix([][]int{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}})
+	test2 := rotateMatrix([][]int{{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}})
+	test3 := rotateMatrix([][]{1,1},{2,2})
+
+	ans1 := [][]int{{1, 1, 1}, {2, 2, 2}, {3, 3, 3}}
+	ans2 := []][]int{{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}, {4, 4, 4, 4}}
+	ans3 := [][]{{2, 1}, {2, 1}}
+	if !cmp.Equal(test1, ans1) {
+		t.Errorf("Expected rotateMatrix to rotate the matrix %v into %v", test1, ans1)
+	}
+
+	if !cmp.Equal(test2, ans2) {
+		t.Errorf("Expected rotateMatrix to rotate the matrix %v into %v", test1, ans1)
+	}
+
+	if !cmp.Equal(test3, ans3) {
+		t.Errorf("Expected rotateMatrix to rotate the matrix %v into %v", test1, ans1)
+	}
+
+}
