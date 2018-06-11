0fb8684f00570783c2f1c45efe19cb604e88b0b7
Leland Krych <Leland.Krych@gmail.com>
2018-06-11 08:52:26 -0700

    Add longestSubstringworepeates solution

--- exercises/exercises_solutions/longest_substring_worepeats_soln.go
+++ exercises/exercises_solutions/longest_substring_worepeats_soln.go
@@ -1,4 +1,17 @@
 // //Given a string, find the length of the longest substring without repeating characters.
-// func longestSubString(sentence string) int {
-
+// func lengthOfLongestSubstring(s string) int {
+// 	bs := []byte(s)
+// 	var maxLen, start int
+// 	idxMap := map[byte]int{}
+// 	for i := 0; i < len(bs); i++ {
+// 			if _, ok := idxMap[bs[i]]; ok && start <= idxMap[bs[i]] {
+// 					start = idxMap[bs[i]] + 1
+// 			} else {
+// 					if maxLen < i - start + 1 {
+// 							maxLen = i - start + 1
+// 					}
+// 			}
+// 			idxMap[bs[i]] = i
+// 	}
+// 	return maxLen
 // }
