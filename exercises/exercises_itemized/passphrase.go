750081f8cb4ec649225c30262d3d3f435e85f9d7
Leland Krych <Leland.Krych@gmail.com>
2018-06-12 08:52:18 -0700

    add clearer instructions to adventPassphrase bonus question

--- exercises/exercises_itemized/passphrase.go
+++ exercises/exercises_itemized/passphrase.go
@@ -1,14 +1,15 @@
 //check to see if a passphrase is valid, it must not contain any duplicate words
 func passPhrase(passphrase string) bool {
-	
+
 }
 
 func passPhraseAdvent() int {
 	//read from ../practice/passphrase_input.txt
+	//each line has a different passphrase, return how many are valid
 }
 
 //bonus, check to see if a passphrase is valid, it must not contain any words that are anagrams of eachother
 // abcde == dcabe
 // func passPhraseAnagram(passphrase string) bool {
-	
+
 // }
