be676347693a92f0f9591d363287aa3011d947fd
Leland Krych <Leland.Krych@gmail.com>
2018-06-12 08:52:38 -0700

    fix function argument registration

--- exercises/exercises_itemized/string_compression.go
+++ exercises/exercises_itemized/string_compression.go
@@ -1,5 +1,5 @@
 //implement a method to perform basic string compression using the counts of repeated chars
 //ex: aabcccccaaa = a2b1c5a3
-func stringCompression(in string) out string {
-	
+func stringCompression(in string) string {
+
 }
