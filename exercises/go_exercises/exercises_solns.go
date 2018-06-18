ae8f78d317ce629bf8a45ac4feca1176388691a8
Leland Krych <Leland.Krych@gmail.com>
2018-06-18 09:37:40 -0700

    add morning calisthenics solutoins

--- exercises/go_exercises/exercises_solns.go
+++ exercises/go_exercises/exercises_solns.go
@@ -1,83 +1,49 @@
 package goExercises
-
-// type stack struct {
-// 	data []int
+ 
+// func digitalRoot(n int) int {
+// 	if n < 10 {
+// 		return n
+// 	}
+// 	return digitalRoot(n%10 + n/10)
 // }
+ 
+// import "math"
 
-// func (s *stack) pop() int {
-// 	popped := s.data[len(s.data)-1]
-// 	s.data = s.data[:len(s.data)-1]
-// 	return popped
-// }
+// //the key insight to this one is noticing that these edits are very similar and that all
+// //we need to do is check to see if the characters are same
+// func oneAway(first, second string) bool {
+// 	//check lengths
+// 	if math.Abs(len(first)-len(second)) > 1 {
+// 		return false
+// 	}
 
-// func (s *stack) push(item int) {
-// 	s.data = append(s.data, item)
-// }
+// 	//get shorter or longer string
+// 	s1 = len(first) < len(second) ? first : second
+// 	s2 = len(first) < len(second) ? second : first
 
-// func (s *stack) peek() int {
-// 	return s.data[len(s.data)-1]
-// }
+// 	idx1 := 0
+// 	idx2 := 0
+// 	foundDiff := false
+// 	for idx2 < len(s2) && idx1 < len(s1) {
+// 		if s1[idx1] != s2[idx2] {
+// 			if foundDiff { //there should only be one edit
+// 				return false
+// 			}
+// 			foundDiff = true
 
-// func (s *stack) isEmpty() bool {
-// 	return len(s.data) == 0
-// }
-
-// func partitionLL(head *Node, partition int) *Node {
-// 	beforeStart := &Node{}
-// 	beforeEnd := &Node{}
-// 	afterStart := &Node{}
-// 	afterEnd := &Node{}
-
-// 	//partition
-// 	currentNode := head
-// 	for currentNode != nil {
-// 		next := currentNode.next
-// 		currentNode.next = nil
-// 		if currentNode.val < partition {
-// 			//insert node into end of before list
-// 			if beforeStart.val == 0 {
-// 				beforeStart = currentNode
-// 				beforeEnd = beforeStart
-// 			} else {
-// 				beforeEnd.next = currentNode
-// 				beforeEnd = currentNode
+// 			if len(s1) == len(s2) {
+// 				idx1++ //on replace, move shorter idx
 // 			}
 // 		} else {
-// 			//insert node into end of after list
-// 			if afterStart.val == 0 {
-// 				afterStart = currentNode
-// 				afterEnd = afterStart
-// 			} else {
-// 				afterEnd.next = currentNode
-// 				afterEnd = currentNode
-// 			}
+// 			idx1++ //if matching, move shorter idx
 // 		}
-// 		currentNode = next
+// 		idx2++ //always move idx for longer
 // 	}
-// 	if beforeStart.val == 0 {
-// 		return afterStart
-// 	}
-
-// 	//merge the two lists
-// 	beforeEnd.next = afterStart
-// 	return beforeStart
+// 	return true
 // }
-
-// func deleteDupsLL(head *Node) *Node {
-// 	intMap := map[int]bool{}
-// 	prev := &Node{}
-// 	currentNode := head
-// 	for currentNode.next != nil {
-// 		if _, ok := intMap[currentNode.val]; ok {
-// 			prev.next = currentNode.next
-// 		} else {
-// 			intMap[currentNode.val] = true
-// 			prev = currentNode
-// 		}
-// 		currentNode = currentNode.next
-// 	}
-// 	if _, ok := intMap[currentNode.val]; ok {
-// 		prev.next = currentNode.next
-// 	}
-// 	return head
+ 
+// //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+// func isUnique(word string) bool {
+// 	//use a map
 // }
+ 
