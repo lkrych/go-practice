c93b34694d3782cffca20011bd58f4d6d0beeea1
Leland Krych <Leland.Krych@gmail.com>
2018-06-18 09:38:06 -0700

    add tests for morning calisthenics

--- exercises/go_exercises/exercises_test.go
+++ exercises/go_exercises/exercises_test.go
@@ -1,205 +1,88 @@
 package goExercises
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestCreatingAStack(t *testing.T) {
-	s := &stack{
-		data: []int{1, 2, 3, 4, 5, 6},
-	}
-	testPop := s.pop()
-	testPop2 := s.pop()
+func TestDigitalRoot(t *testing.T) {
+	test1 := digitalRoot(10)
+	test2 := digitalRoot(83)
+	test3 := digitalRoot(123)
+	test4 := digitalRoot(789)
+	test5 := digitalRoot(452982)
 
-	if len(s.data) != 4 {
-		t.Errorf("Expected pop to return elements from the top of the stack: %v, popped: %v", s.data, testPop)
+	if test1 != 1 {
+		t.Errorf("Expected digitalRoot of 10 to be 1, not %v", test1)
 	}
-	if s.peek() != 4 {
-		t.Errorf("Expected peek to return the element at the top of the stack, not %v", s.peek())
-	}
-	if len(s.data) != 4 {
-		t.Errorf("Expected peek to not delete elements from the top of the stack")
-	}
-	if s.isEmpty() {
-		t.Errorf("The isEmpty function doesnt work because the stack has %v elements", len(s.data))
+	if test2 != 2 {
+		t.Errorf("Expected digitalRoot of 83 to be 2, not %v", test2)
 	}
 
-	s.push(testPop)
-	if len(s.data) != 5 {
-		t.Errorf("Expected pop to return elements from the top of the stack: %v, popped: %v", s.data, testPop2)
-	}
-	if s.peek() != 6 {
-		t.Errorf("Expected peek to return the element at the top of the stack, not %v", s.peek())
-	}
-	if len(s.data) != 5 {
-		t.Errorf("Expected peek to not delete elements from the top of the stack")
-	}
-	if s.isEmpty() {
-		t.Errorf("The isEmpty function doesnt work because the stack has %v elements\n", len(s.data))
+	if test3 != 6 {
+		t.Errorf("Expected digitalRoot of 123 to be 6, not %v", test3)
 	}
 
-	s.push(testPop2)
-	if len(s.data) != 6 {
-		t.Errorf("Expected push to return elements to the top of the stack")
-	}
-	if s.peek() != 5 {
-		t.Errorf("Expected peek to return the element at the top of the stack, not %v", s.peek())
-	}
-	if len(s.data) != 6 {
-		t.Errorf("Expected peek to not delete elements from the top of the stack")
-	}
-	if s.isEmpty() {
-		t.Errorf("The isEmpty function doesnt work because the stack has %v elements\n", len(s.data))
+	if test4 != 6 {
+		t.Errorf("Expected digitalRoot of 789 to be 6, not %v", test4)
 	}
 
-	for i := 0; i < 6; i++ {
-		s.pop()
-	}
-	if !s.isEmpty() {
-		t.Errorf("The isEmpty function doesnt work because the stack should be empty")
+	if test5 != 3 {
+		t.Errorf("Expected digitalRoot of 452982 to be 3, not %v", test5)
 	}
 }
 
-func TestCreateALinkedList(t *testing.T) {
-	input1 := []int{10, 5, 8, 1, 3, 2, 4}
-	input2 := []int{4, 7, 2, 3, 1}
-	input3 := []int{105, 783, 98, 77, 33, 102}
+func TestOneAway(t *testing.T) {
+	test1 := oneAway("pale", "ple")
+	test2 := oneAway("pales", "pale")
+	test3 := oneAway("pale", "bale")
+	test4 := oneAway("pale", "bake")
+	test5 := oneAway("baleen", "baleen")
 
-	list1 := createALinkedList(input1)
-	list2 := createALinkedList(input2)
-	list3 := createALinkedList(input3)
-
-	partition1 := partitionLL(list1, 5)
-	partition2 := partitionLL(list2, 3)
-	partition3 := partitionLL(list3, 105)
-
-	if findIdxOfVal(partition1, 4) > 5 {
-		t.Errorf("Expected partition to adjust the position of nodes")
-		printLL(partition1)
+	if test1 != true {
+		t.Errorf("Expected oneAway of tact coa to be true, not %v", test1)
 	}
 
-	if findIdxOfVal(partition1, 10) < 4 {
-		t.Errorf("Expected partition to adjust the position of nodes")
-		printLL(partition1)
+	if test2 != true {
+		t.Errorf("Expected oneAway of aaa to be true, not %v", test2)
 	}
 
-	if findIdxOfVal(partition2, 7) < 3 {
-		t.Errorf("Expected partition to adjust the position of nodes")
-		printLL(partition2)
+	if test3 != true {
+		t.Errorf("Expected oneAway of abab to be true, not %v", test3)
 	}
 
-	if findIdxOfVal(partition1, 2) > 3 {
-		t.Errorf("Expected partition to adjust the position of nodes")
-		printLL(partition2)
+	if test4 != false {
+		t.Errorf("Expected oneAway of cadac to be false, not %v", test4)
 	}
 
-	if findIdxOfVal(partition3, 102) > 5 {
-		t.Errorf("Expected partition to adjust the position of nodes")
-		printLL(partition3)
-	}
-
-	if findIdxOfVal(partition3, 783) < 3 {
-		t.Errorf("Expected partition to adjust the position of nodes")
-		printLL(partition3)
-	}
-
-}
-
-func findIdxOfVal(head *Node, search int) int {
-	idx := 0
-	for head.val != search {
-		idx++
-		head = head.next
-	}
-	return idx
-}
-
-func createALinkedList(input []int) *Node {
-	head := &Node{
-		val:  input[0],
-		next: nil,
-	}
-	currentNode := head
-	for i := 1; i < len(input); i++ {
-		nextNode := &Node{
-			val:  input[i],
-			next: nil,
-		}
-		currentNode.next = nextNode
-		currentNode = nextNode
-	}
-	return head
-}
-
-func printLL(head *Node) {
-	currentNode := head
-
-	for currentNode.next != nil {
-		fmt.Printf("  %v  ->", currentNode)
-		currentNode = currentNode.next
-	}
-	fmt.Printf("  %v  \n", currentNode)
-}
-
-func TestRemoveDuplicatesLL(t *testing.T) {
-	input1 := []int{1, 2, 3, 3, 4, 4, 5}
-	input2 := []int{5, 8, 6, 7, 8, 9, 7, 10}
-	input3 := []int{100, 102, 101, 102, 101, 102}
-
-	list1 := createALinkedList(input1)
-	list2 := createALinkedList(input2)
-	list3 := createALinkedList(input3)
-
-	list1 = deleteDupsLL(list1)
-	list2 = deleteDupsLL(list2)
-	list3 = deleteDupsLL(list3)
-
-	len1 := countLengthOfLL(list1)
-	len2 := countLengthOfLL(list2)
-	len3 := countLengthOfLL(list3)
-
-	if len1 != 5 {
-		t.Errorf("Expected deleteDups to make a list of length 5 not %v", len1)
-		fmt.Println("Printing list1")
-		printLL(list1)
-	}
-
-	if len2 != 6 {
-		t.Errorf("Expected deleteDups to make a list of length 6 not %v", len2)
-		fmt.Println("Printing list2")
-		printLL(list2)
-	}
-
-	if len3 != 3 {
-		t.Errorf("Expected deleteDups to make a list of length 3 not %v", len3)
-		fmt.Println("Printing list3")
-		printLL(list3)
+	if test5 != true {
+		t.Errorf("Expected oneAway of abcde to be true, not %v", test5)
 	}
 }
 
-func countLengthOfLL(head *Node) int {
-	count := 0
-	currentNode := head
-	for currentNode.next != nil {
-		count++
-		currentNode = currentNode.next
-	}
-	count++ //b/c you won't count the last one
-	return count
-}
+func TestisUnique(t *testing.T) {
+	test1 := isUnique("blah")
+	test2 := isUnique("banana")
+	test3 := isUnique("fike hyjrant")
+	test4 := isUnique("fire hydrant")
+	test5 := isUnique("Another Day")
 
-func findValueInLL(head *Node, findVal int) bool {
-	found := false
-	currentNode := head
-	for currentNode.next != nil {
-		if currentNode.val == findVal {
-			found = true
-		}
-		currentNode = currentNode.next
+	if test1 != true {
+		t.Errorf("Expected blah to be true because all letters are unique ")
 	}
-	if currentNode.val == findVal { //catch the last node
-		found = true
+
+	if test2 != false {
+		t.Errorf("Expected banana to be false because all letters are not unique ")
 	}
-	return found
+
+	if test3 != true {
+		t.Errorf("Expected fike hyjrant to be true because all letters are unique ")
+	}
+
+	if test4 != false {
+		t.Errorf("Expected fire hydrant to be false because all letters are not unique ")
+	}
+
+	if test5 != false {
+		t.Errorf("Expected Another day to be false because all letters are not unique ")
+	}
 }
