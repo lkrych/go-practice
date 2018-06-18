db7fd4f2655df0129b1ab98cb4baee35454cf1bb
Leland Krych <Leland.Krych@gmail.com>
2018-06-18 09:19:25 -0700

    add morning calisthetnic solutions

--- exercises/go_exercises/exercises_solns.go
+++ exercises/go_exercises/exercises_solns.go
@@ -1,120 +1,83 @@
 package goExercises
- 
-func deleteDupsLL(head *Node) *Node {
-	intMap := map[int]bool{}
-	prev := &Node{}
-	currentNode := head
-	for currentNode.next != nil {
-		if _, ok := intMap[currentNode.val]; ok {
-			prev.next = currentNode.next
-		} else {
-			intMap[currentNode.val] = true
-			prev = currentNode
-		}
-		currentNode = currentNode.next
-	}
-	if _, ok := intMap[currentNode.val]; ok {
-		prev.next = currentNode.next
-	}
-	return head
-}
- 
-// import "strings"
 
-// func scrambleString(word string, idxArray []int) string {
-// 	split := strings.Split(word, "")
-// 	scrambled := []string{}
-// 	for _, el := range idxArray {
-// 		scrambled = append(scrambled, split[el])
-// 	}
-// 	return strings.Join(scrambled, "")
-// }
- 
-// func partitionEvenOdd(arr []int) []int {
-// 	partition := 0
-// 	for i := 0; i < len(arr); i++ {
-// 		if arr[i]%2 == 0 {
-// 			arr[i], arr[partition] = arr[partition], arr[i]
-// 			partition++
-// 		}
-// 	}
-// 	front := quickSort(arr[:partition])
-// 	back := quickSort(arr[partition:])
-// 	return append(front, back...)
+// type stack struct {
+// 	data []int
 // }
 
-// func quickSort(arr []int) []int {
-// 	if len(arr) <= 1 {
-// 		return arr
-// 	}
+// func (s *stack) pop() int {
+// 	popped := s.data[len(s.data)-1]
+// 	s.data = s.data[:len(s.data)-1]
+// 	return popped
+// }
 
-// 	pivot := arr[0]
-// 	left := []int{}
-// 	right := []int{}
+// func (s *stack) push(item int) {
+// 	s.data = append(s.data, item)
+// }
 
-// 	for _, val := range arr[1:] {
-// 		if val >= pivot {
-// 			right = append(right, val)
+// func (s *stack) peek() int {
+// 	return s.data[len(s.data)-1]
+// }
+
+// func (s *stack) isEmpty() bool {
+// 	return len(s.data) == 0
+// }
+
+// func partitionLL(head *Node, partition int) *Node {
+// 	beforeStart := &Node{}
+// 	beforeEnd := &Node{}
+// 	afterStart := &Node{}
+// 	afterEnd := &Node{}
+
+// 	//partition
+// 	currentNode := head
+// 	for currentNode != nil {
+// 		next := currentNode.next
+// 		currentNode.next = nil
+// 		if currentNode.val < partition {
+// 			//insert node into end of before list
+// 			if beforeStart.val == 0 {
+// 				beforeStart = currentNode
+// 				beforeEnd = beforeStart
+// 			} else {
+// 				beforeEnd.next = currentNode
+// 				beforeEnd = currentNode
+// 			}
 // 		} else {
-// 			left = append(left, val)
+// 			//insert node into end of after list
+// 			if afterStart.val == 0 {
+// 				afterStart = currentNode
+// 				afterEnd = afterStart
+// 			} else {
+// 				afterEnd.next = currentNode
+// 				afterEnd = currentNode
+// 			}
 // 		}
+// 		currentNode = next
 // 	}
-// 	answer := []int{}
-// 	sortedLeft := quickSort(left)
-// 	sortedRight := quickSort(right)
-// 	answer = append(answer, sortedLeft...)
-// 	answer = append(answer, pivot)
-// 	answer = append(answer, sortedRight...)
-// 	return answer
+// 	if beforeStart.val == 0 {
+// 		return afterStart
+// 	}
+
+// 	//merge the two lists
+// 	beforeEnd.next = afterStart
+// 	return beforeStart
 // }
- 
-// import "math"
 
-// func multiplyArb(arr1 []int, arr2 []int) []int {
-// 	sign := 1
-// 	//check if numbers are negative
-// 	if (arr1[0] * arr2[0]) > 0 {
-// 		sign = 1
-// 	} else {
-// 		sign = -1
-// 	}
-// 	//set to absolute val
-// 	arr1[0] = int(math.Abs(float64(arr1[0])))
-// 	arr2[0] = int(math.Abs(float64(arr2[0])))
-
-// 	total := make([]int, len(arr1)+len(arr2))
-// 	for i := len(arr1) - 1; i >= 0; i-- {
-// 		for j := len(arr2) - 1; j >= 0; j-- {
-// 			total[i+j+1] += arr1[i] * arr2[j]
-// 			total[i+j] += total[i+j+1] / 10
-// 			total[i+j+1] %= 10
+// func deleteDupsLL(head *Node) *Node {
+// 	intMap := map[int]bool{}
+// 	prev := &Node{}
+// 	currentNode := head
+// 	for currentNode.next != nil {
+// 		if _, ok := intMap[currentNode.val]; ok {
+// 			prev.next = currentNode.next
+// 		} else {
+// 			intMap[currentNode.val] = true
+// 			prev = currentNode
 // 		}
+// 		currentNode = currentNode.next
 // 	}
-// 	zeroIdx := 0
-// 	for {
-// 		if total[zeroIdx] != 0 {
-// 			break
-// 		}
-// 		zeroIdx++
+// 	if _, ok := intMap[currentNode.val]; ok {
+// 		prev.next = currentNode.next
 // 	}
-// 	total = total[zeroIdx:]
-// 	total[0] = total[0] * sign
-
-// 	return total
+// 	return head
 // }
- 
-// import "math"
-
-// func isPowerOfTwo(n float64) bool {
-// 	power := float64(0)
-// 	value := math.Exp2(power)
-// 	for value <= n {
-// 		if value == n {
-// 			return true
-// 		}
-// 		power++
-// 		value = math.Exp2(power)
-// 	}
-// 	return false
-// }
- 
