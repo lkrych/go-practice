c14c2fefd3660e088b35b91f0ac6fc7cdf62c053
Leland Krych <Leland.Krych@gmail.com>
2018-07-09 11:44:08 -0600

    update fib soln and tests

--- exercises/exercises_solutions/fibonacci_soln.go
+++ exercises/exercises_solutions/fibonacci_soln.go
@@ -1,9 +1,14 @@
-func fibonacci(n int) []int {
-	fibArr := []int{}
-	if n < 2 {
-		fibArr = append(fibArr, n)
-		return fibArr
-	}
-	fibArr = append(fibArr, fibonacci(n-1)+fibonacci(n-2))
-	return fibArr
-}
+// import "errors"
+
+// func fibonacci(n int) ([]int, error) {
+// 	if n < 1 {
+// 		return nil, errors.New("n must be greater than 0")
+// 	}
+// 	fibArr := []int{}
+// 	if n < 2 {
+// 		fibArr = append(fibArr, n)
+// 		return fibArr, nil
+// 	}
+// 	fibArr = append(fibArr, (fibArr[n-1] + fibArr[n-2]))
+// 	return fibArr, nil
+// }
--- exercises/exercises_test/fibonacci_test.go
+++ exercises/exercises_test/fibonacci_test.go
@@ -1,13 +1,12 @@
-import "testing"
 
 func testFibonacci(t *testing.T) {
-	t1 := fibonacci(3)
-	t2 := fibonacci(6)
-	t3 := fibonacci(7)
-	t4 := fibonacci(9)
-	t5 := fibonacci(15)
-	t6 := fibonacci(-1)
-	t7 := fibonacci(0)
+	t1, err1 := fibonacci(3)
+	t2, err2 := fibonacci(6)
+	t3, err3 := fibonacci(7)
+	t4, err4 := fibonacci(9)
+	t5, err5 := fibonacci(15)
+	_, err6 := fibonacci(-1)
+	_, err7 := fibonacci(0)
 	ans1 := []int{1, 1, 2}
 	ans2 := []int{1, 1, 2, 3, 5, 8}
 	ans3 := []int{1, 1, 2, 3, 5, 8, 13}
@@ -19,27 +18,46 @@ func testFibonacci(t *testing.T) {
 		t.Errorf("Expected the fibonacci sequence of %v to be %v not %v", 3, ans1, t1)
 	}
 
+	if err1 != nil {
+		t.Errorf("There was not supposed to be an error for t1")
+	}
+
 	if !cmp.Equal(t2, ans2) {
 		t.Errorf("Expected the fibonacci sequence of %v to be %v not %v", 6, ans2, t2)
+	}
+
+	if err2 != nil {
+		t.Errorf("There was not supposed to be an error for t2")
 	}
 
 	if !cmp.Equal(t3, ans3) {
 		t.Errorf("Expected the fibonacci sequence of %v to be %v not %v", 7, ans3, t3)
 	}
 
+	if err3 != nil {
+		t.Errorf("There was not supposed to be an error for t3")
+	}
+
 	if !cmp.Equal(t4, ans4) {
 		t.Errorf("Expected the fibonacci sequence of %v to be %v not %v", 9, ans4, t4)
+	}
+
+	if err4 != nil {
+		t.Errorf("There was not supposed to be an error for t4")
 	}
 
 	if !cmp.Equal(t5, ans5) {
 		t.Errorf("Expected the fibonacci sequence of %v to be %v not %v", 15, ans5, t5)
 	}
+	if err5 != nil {
+		t.Errorf("There was not supposed to be an error for t5")
+	}
 
-	if t6.Error() != errorMessage {
+	if err6.Error() != errorMessage {
 		t.Errorf("Expected the function to throw an error for input less than 1")
 	}
 
-	if t7.Error() != errorMessage {
+	if err7.Error() != errorMessage {
 		t.Errorf("Expected the function to throw an error for input less than 1")
 	}
 }
