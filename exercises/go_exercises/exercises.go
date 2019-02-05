f72a66265f73357e2bdc8211ec6ff69f9b85b31c
Leland Krych <Leland.Krych@gmail.com>
2019-02-05 08:04:54 -0800

    keep truckin'

--- exercises/go_exercises/exercises.go
+++ exercises/go_exercises/exercises.go
@@ -1,48 +1,71 @@
 package goExercises
 
-//take in an array of digits encoding a decimal, d and update the array to rep d + 1
-//ex: 1.29 => 1.30 ~ [1,2,9] => [1,3,0]
-//ex 1.48372 => 1.48373
-//TAKE AWAY:  This algorithm should work in a language that has finite-precision arithmetic
-//Arrays can be used to break up numbers of arbitrary size, this allows us to handle very big numbers!
-func incrementArb(arr []int) []int {
-	carry := 0
-	for i := len(arr) - 1; i >= 0; i-- {
-		currentIdx := arr[i]
-		currentSum := 0
-		if i == len(arr)-1 {
-			currentSum = carry + currentIdx + 1
+import ("strings"; "fmt")
+//given a string, write a function to check it is a permutation of a palindrome
+func palindromePermutation(perm string) bool {
+	sorted := quickSort(strings.Split(perm, ""))
+	charMap := make(map[string]int)
+	fmt.Println("The sorted input is", sorted)
+	//sort the chars and iterate through them
+	for _, char := range sorted {
+		if char == " " { continue }
+		if count, ok := charMap[char]; ok {
+			charMap[char] = count + 1
 		} else {
-			currentSum = carry + currentIdx
-		}
-		carry = currentSum / 10
-		arr[i] = currentSum % 10
-	}
-
-	if carry != 0 {
-		arr = append([]int{1}, arr...)
-	}
-
-	return arr
-
-}
-
-//given a linked list, delete nodes with the the value of x
-//optional create a function that builds a linked list with an input array of integers
-//if you don't want to do this, you can just look in the test file for this problem and grab one
-func deleteANode(list *Node, el int) *Node {
-	currentHead := list
-	currentNode := list
-	if currentNode.val == el {
-		currentHead = currentHead.next
-	}
-	for currentNode.next != nil {
-		pastNode := currentNode
-		currentNode = currentNode.next
-		if currentNode.val == el {
-			pastNode.next = currentNode.next
+			charMap[char] = 1
 		}
 	}
-
-	return currentHead
+	foundOdd := false
+	fmt.Printf("charMap is %v for word %v \n", charMap, perm)
+	for _, v := range charMap {
+		
+		if v % 2 != 0 {
+			if foundOdd {
+				//there can't be more than one odd count
+				return false
+			} else {
+				foundOdd = true
+			}
+		}
+	}
+	return true
 }
+
+func quickSort(arr []string) []string {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	pivot := arr[0]
+	left := []string{}
+	right := []string{}
+
+	for i := 1; i < len(arr); i++ {
+		if arr[i] >= pivot {
+			right = append(right, arr[i])
+		} else {
+			left = append(left, arr[i])
+		}
+	}
+
+	sortedLeft := quickSort(left)
+	sortedRight := quickSort(right)
+	sorted := []string{}
+
+	sorted = append(sorted, sortedLeft...)
+	sorted = append(sorted, pivot)
+	sorted = append(sorted, sortedRight...)
+
+	return sorted
+} 
+ 
+//return if the word is a palindrome
+func isPalindrome(word string) bool {
+	for i,j := 0, len(word) -1; i < j; i,j = i+1, j-1 {
+		if word[i] != word[j] {
+			return false
+		}
+	}
+	return true
+}
+ 
--- exercises/go_exercises/exercises_solns.go
+++ exercises/go_exercises/exercises_solns.go
@@ -1,50 +1,69 @@
 package goExercises
  
-// func incrementArb(arr []int) []int {
-// 	lastIdx := len(arr) - 1
-// 	arr[lastIdx] = (arr[lastIdx] + 1) % 10
-// 	if arr[lastIdx] == 0 {
-// 		arr = append(incrementArb(arr[:lastIdx]), arr[lastIdx])
+//there are two ways to do this:
+//one contruct a hash and keep a count of all the characters you see
+//if there are two odd counts, this is not a possible palindrome
+//big O = O(n)
+
+//the other solution might be a little more space-efficient, you use a bit
+//vector to model the alphabet, you toggle the index of the char when you see it
+//you should only have one 1
+// func palindromePerm(perm string) bool {
+// 	charMap := map[char]int{}
+// 	for i := 0; i < len(perm); i++ {
+// 		char := perm[i]
+// 		if count, ok := charMap[char]; ok {
+// 			charMap[char]++
+// 		} else {
+// 			charMap[char] = 1
+// 		}
 // 	}
-// 	return arr
+// 	oddCount := 0
+// 	for key, value := range charMap {
+// 		if value%2 != 0 {
+// 			oddCount++
+// 		}
+// 	}
+// 	return oddCount <= 1
+// }
+
+// func palindromePermBit(perm string) bool {
+// 	bitVector := createBitVector(len(perm))
+// 	alpha := "abcdefghijklmnopqrstuvwxyz"
+// 	for _, char := strings.Split(perm, "") {
+// 		if char == " " { //skip spaces
+// 			continue
+// 		}
+// 		idx := strings.Index(alpha, char)
+// 		bitVector[idx] = bitVector[idx] == 1 ? 0 : 1
+// 	}
+// 	oddCount := 0
+// 	for _, el := range bitVector {
+// 		if el == 1 {
+// 			oddCount+++
+// 		}
+// 	}
+// 	return oddCount <= 1
+
+// }
+
+// func createBitVector(len int) []int {
+// 	bitVector := make([]int, len)
+// 	for i := 0; i < len; i++ {
+// 		bitVector[i] = 0
+// 	}
 // }
  
-// type Node struct {
-// 	val  int
-// 	next *Node
-// }
-
-// //if you created your own function, comment this out!
-// func createALinkedList(input []int) *Node {
-// 	head := &Node{
-// 		val:  input[0],
-// 		next: nil,
-// 	}
-// 	currentNode := head
-// 	for i := 1; i < len(input); i++ {
-// 		nextNode := &Node{
-// 			val:  input[i],
-// 			next: nil,
+// func isPalindrome(word string) bool {
+// 	idx1 := 0
+// 	idx2 := len(word) - 1
+// 	for idx1 < idx2 {
+// 		if word[idx1] != word[idx2] {
+// 			return false
 // 		}
-// 		currentNode.next = nextNode
-// 		currentNode = nextNode
+// 		idx1++
+// 		idx2--
 // 	}
-// 	return head
+// 	return true
 // }
-
-// func deleteANode(head *Node, deleteVal int) *Node {
-// 	if head.val == deleteVal { //in case the head is the value we want to delete!
-// 		head = head.next
-// 	}
-// 	currentNode := head
-// 	for currentNode.next != nil {
-// 		if currentNode.next.val == deleteVal {
-// 			currentNode.next = currentNode.next.next
-// 		}
-// 		currentNode = currentNode.next
-// 	}
-
-// 	return head
-// }
-
  
